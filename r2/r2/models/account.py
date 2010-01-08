760090e8872fabebf5818cedc36e89512d970591
Mike <raldi@reddit.com>
2010-01-08 12:20:39 -0800

    Also, a safeguard to keep people (e.g., me) from accidentally calling cup_info_multi() with an Account rather than an Account._id

--- r2/r2/models/account.py
+++ r2/r2/models/account.py
@@ -308,6 +308,7 @@ class Account(Thing):
 
     @classmethod
     def cup_info_multi(cls, ids):
+        ids = [ int(id) for id in ids ]
         return sgm(g.hardcache, ids, miss_fn=None, prefix="cup_info-")
 
 class FakeAccount(Account):
