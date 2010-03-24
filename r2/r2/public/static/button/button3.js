3a5bff7cd46ce72992e5c9d6bbfeb4ba217b5634
KeyserSosa <chris@reddit.com>
2010-03-24 13:05:13 -0700

    button fix

--- r2/r2/public/static/button/button3.js
+++ r2/r2/public/static/button/button3.js
@@ -9,6 +9,9 @@ var write_string="<iframe src=\"http://www.reddit.com/static/button/button3.html
   if (window.reddit_title) {
        write_string += '&title=' + encodeURIComponent(window.reddit_title);
   }
+  if (window.reddit_target) {
+       write_string += '&sr=' + encodeURIComponent(window.reddit_target);
+  }
   if (window.reddit_css) {
       write_string += '&css=' + encodeURIComponent(window.reddit_css);
   }
