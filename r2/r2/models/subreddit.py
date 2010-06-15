cf9da39d978d7da26ccf94e1c1e5559fa5ab2e6f
KeyserSosa <chris@reddit.com>
2010-06-15 15:10:21 -0700

    randomize that expiration time of the subreddit query

--- r2/r2/models/subreddit.py
+++ r2/r2/models/subreddit.py
@@ -351,7 +351,7 @@ class Subreddit(Thing, Printable):
                                        data = True,
                                        read_cache = True,
                                        write_cache = True,
-                                       cache_time = 20 * 60)
+                                       cache_time = (15 + random.random(10)) * 60)
         if lang != 'all':
             pop_reddits._filter(Subreddit.c.lang == lang)
 
