4342ae76cd2fa5fe2a87a808a92eb0f1f3b8c2ca
ketralnis <ketralnis@reddit.com>
2010-06-16 17:21:54 -0700

    Fix string conversion in sgm

--- r2/r2/lib/sgm.pyx
+++ r2/r2/lib/sgm.pyx
@@ -7,11 +7,13 @@ def sgm(cache, keys, miss_fn, str prefix='', int time=0):
     cdef dict cached
     cdef dict calculated
     cdef dict calculated_to_cache
-    cdef set  s_need
-    cdef list k_need
+    cdef set  still_need
 
     ret = {}
 
+    # map the string versions of the keys to the real version. we only
+    # need this to interprate the cache's response and turn it back
+    # into the version they asked for
     s_keys = {}
     for key in keys:
         s_keys[str(key)] = key
@@ -21,15 +23,12 @@ def sgm(cache, keys, miss_fn, str prefix='', int time=0):
         ret[s_keys[k]] = v
 
     if miss_fn and len(cached) < len(s_keys):
-        # if we didn't get all of the keys from the cache. take the
-        # missing subset
-        s_need = set(s_keys.keys()) - set(ret.keys())
+        # if we didn't get all of the keys from the cache, go to the
+        # miss_fn with the keys they asked for minus the ones that we
+        # found
+        still_need = set(s_keys.values()) - set(ret.keys())
 
-        k_need = []
-        for i in s_need:
-            k_need.append(s_keys[i])
-
-        calculated = miss_fn(k_need)
+        calculated = miss_fn(still_need)
         ret.update(calculated)
 
         calculated_to_cache = {}
