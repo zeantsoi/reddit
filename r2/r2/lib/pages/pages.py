a1d4b90db9df3793faa7710052d6e1198eaf0012
Mike <raldi@reddit.com>
2010-08-18 14:18:15 -0700

    turn off gold sidebar ad

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -225,7 +225,7 @@ class Reddit(Templated):
                               subtitles = rand_strings.get("create_reddit", 2),
                               show_cover = True, nocname=True))
 
-        if True and not c.user.gold and self.submit_box:
+        if False and not c.user.gold and self.submit_box:
             ps.append(SideBox(_('New subscriber features'),
                               'http://blog.reddit.com/2010/08/feature-out-feature-in.html',
                               'gold',
