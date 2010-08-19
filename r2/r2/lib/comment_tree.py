f975e011ff058f02e47ac5c56f6d02a101a6b321
ketralnis <ketralnis@reddit.com>
2010-08-19 15:01:24 -0700

    Respect the read-only apps

--- r2/r2/lib/comment_tree.py
+++ r2/r2/lib/comment_tree.py
@@ -221,7 +221,9 @@ def link_comments_and_sort(link_id, sort):
         g.log.warning(
             "Error in comment_tree: sorter %r inconsistent (missing %d e.g. %r)"
             % (sort_comments_key(link_id, sort), len(sorter_needed), sorter_needed[:10]))
-        update_comment_votes(Comment._byID(sorter_needed, return_dict=False))
+        if not g.disallow_db_writes:
+            update_comment_votes(Comment._byID(sorter_needed, return_dict=False))
+
         sorter.update(_comment_sorter_from_cids(sorter_needed, sort))
 
     # load the parents
