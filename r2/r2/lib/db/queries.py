70d1a6f848bd0ed10ab54855ea0918aaceee98fa
ketralnis <ketralnis@reddit.com>
2010-09-23 11:49:28 -0700

    Single votes

--- r2/r2/lib/db/queries.py
+++ r2/r2/lib/db/queries.py
@@ -1024,7 +1024,7 @@ def process_votes_multi(limit=100):
 
     amqp.handle_items('register_vote_q', _handle_vote, limit = limit)
 
-process_votes = process_votes_multi
+process_votes = process_votes_single
 
 def process_comment_sorts(limit=500):
     def _handle_sort(msgs, chan):
