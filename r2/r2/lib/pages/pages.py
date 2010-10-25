4641173756d41be970aa40c44a17d0d90a545cce
Mike <raldi@reddit.com>
2010-10-25 12:29:47 -0700

    Move opera tracker to /r/operabrowser

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -218,9 +218,8 @@ class Reddit(Templated):
                 ps.append(Ads())
             no_ads_yet = False
 
-        if (request.fullpath == '/' and
-            (c.user.pref_show_sponsorships or not c.user.gold) and
-            not c.cname):
+        if (request.fullpath.lower().startswith('/r/operabrowser') and
+            (c.user.pref_show_sponsorships or not c.user.gold)):
             ps.append(unsafe("<iframe id='opera-ad-frame' frameborder='0' scrolling='no' name='opera-ad-frame' src='http://www.opera.com/portal/reddit/banner/' style='height: 100px; width: 300px;'></iframe>"))
 
         if self.submit_box:
