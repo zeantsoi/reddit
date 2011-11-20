c662c7f8fc11570a6f1373e560cbdaa666f70dd0
Neil Williams <neil@reddit.com>
2011-11-20 12:51:30 -0800

    Snudown v1.0.2 supports git:// schemes.

--- r2/setup.py
+++ r2/setup.py
@@ -82,11 +82,11 @@ setup(
         "pylibmc==1.2.1-dev",
         "py-bcrypt",
         "python-statsd",
-        "snudown",
+        "snudown==1.0.2",
     ],
     dependency_links=[
         "https://github.com/downloads/reddit/pylibmc/pylibmc-1.2.1-dev.tar.gz#egg=pylibmc-1.2.1-dev",
-        "https://nodeload.github.com/reddit/snudown/tarball/v1.0.0#egg=snudown-1.0.0",
+        "https://nodeload.github.com/reddit/snudown/tarball/v1.0.2#egg=snudown-1.0.2",
     ],
     packages=find_packages(exclude=["ez_setup"]),
     cmdclass=commands,
