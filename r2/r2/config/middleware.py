zeantsoi/reddit

commit 825e644f526a5570590c4648e28c1beda4d79e17

Move extension mapping to a config module.

r2/r2/config/middleware.py
@@ -32,6 +32,7 @@ from pylons.wsgiapp import PylonsApp, PylonsBaseWSGIApp
 
 from r2.config.environment import load_environment
 from r2.config.rewrites import rewrites
+from r2.config.extensions import extension_mapping, set_extension
 from r2.lib.utils import rstrips, is_authorized_cname
 from r2.lib.jsontemplates import api_type
 
@@ -371,35 +372,16 @@ class DomainListingMiddleware(object):
 
 class ExtensionMiddleware(object):
     ext_pattern = re.compile(r'\.([^/]+)\Z')
-
-    extensions = (('rss' , ('xml', 'text/xml; charset=UTF-8')),
-                  ('xml' , ('xml', 'text/xml; charset=UTF-8')),
-                  ('js' , ('js', 'text/javascript; charset=UTF-8')),
-                  ('wired' , ('wired', 'text/javascript; charset=UTF-8')),
-                  ('embed' , ('htmllite', 'text/javascript; charset=UTF-8')),
-                  ('mobile' , ('mobile', 'text/html; charset=UTF-8')),
-                  ('png' , ('png', 'image/png')),
-                  ('css' , ('css', 'text/css')),
-                  ('csv' , ('csv', 'text/csv; charset=UTF-8')),
-                  ('api' , (api_type(), 'application/json; charset=UTF-8')),
-                  ('json-html' , (api_type('html'), 'application/json; charset=UTF-8')),
-                  ('json-compact' , (api_type('compact'), 'application/json; charset=UTF-8')),
-                  ('compact' , ('compact', 'text/html; charset=UTF-8')),
-                  ('json' , (api_type(), 'application/json; charset=UTF-8')),
-                  ('i' , ('compact', 'text/html; charset=UTF-8')),
-                  )
-
+    
     def __init__(self, app):
         self.app = app
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
         domain_ext = environ.get('reddit-domain-extension')
-        for ext, val in self.extensions:
+        for ext, val in extension_mapping.iteritems():
             if ext == domain_ext or path.endswith('.' + ext):
-                environ['extension'] = ext
-                environ['render_style'] = val[0]
-                environ['content_type'] = val[1]
+                set_extension(environ, ext)
                 #strip off the extension
                 if path.endswith('.' + ext):
                     environ['PATH_INFO'] = path[:-(len(ext) + 1)]

r2/r2/config/extensions.py
@@ -1,0 +1,23 @@
+from r2.lib.jsontemplates import api_type
+
+extension_mapping = {
+    "rss": ("xml", "text/xml; charset=UTF-8"),
+    "xml": ("xml", "text/xml; charset=UTF-8"),
+    "js": ("js", "text/javascript; charset=UTF-8"),
+    "wired": ("wired", "text/javascript; charset=UTF-8"),
+    "embed": ("htmllite", "text/javascript; charset=UTF-8"),
+    "mobile": ("mobile", "text/html; charset=UTF-8"),
+    "png": ("png", "image/png"),
+    "css": ("css", "text/css"),
+    "csv": ("csv", "text/csv; charset=UTF-8"),
+    "api": (api_type(), "application/json; charset=UTF-8"),
+    "json-html": (api_type("html"), "application/json; charset=UTF-8"),
+    "json-compact": (api_type("compact"), "application/json; charset=UTF-8"),
+    "compact": ("compact", "text/html; charset=UTF-8"),
+    "json": (api_type(), "application/json; charset=UTF-8"),
+    "i": ("compact", "text/html; charset=UTF-8"),
+}
+
+def set_extension(environ, ext):
+    environ["extension"] = ext
+    environ["render_style"], environ["content_type"] = extension_mapping[ext]

commit 53e4d510e1a910a0c14636cbff4ee63435afe7b4

Simplify and fix extension mapping lookups.
This fixes an issue introduced by
825e644f526a5570590c4648e28c1beda4d79e17 in which the extension mapping
changes from a tuple to a dict. Since dicts don't have a guaranteed
ordering, that change affected the behavior of the .json-compact extension.

r2/r2/config/middleware.py
@@ -378,14 +378,19 @@ class ExtensionMiddleware(object):
 
     def __call__(self, environ, start_response):
         path = environ['PATH_INFO']
+        fname, sep, path_ext = path.rpartition('.')
         domain_ext = environ.get('reddit-domain-extension')
-        for ext, val in extension_mapping.iteritems():
-            if ext == domain_ext or path.endswith('.' + ext):
-                set_extension(environ, ext)
-                #strip off the extension
-                if path.endswith('.' + ext):
-                    environ['PATH_INFO'] = path[:-(len(ext) + 1)]
-                break
+
+        ext = None
+        if path_ext in extension_mapping:
+            ext = path_ext
+            # Strip off the extension.
+            environ['PATH_INFO'] = path[:-(len(ext) + 1)]
+        elif domain_ext in extension_mapping:
+            ext = domain_ext
+
+        if ext:
+            set_extension(environ, ext)
         else:
             environ['render_style'] = 'html'
             environ['content_type'] = 'text/html; charset=UTF-8'