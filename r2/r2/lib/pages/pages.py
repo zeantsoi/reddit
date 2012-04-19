40a9160b1096728ed34d234ceca96535e11e331e
Neil Williams <neil@reddit.com>
2012-04-19 14:15:12 -0700

    Fix wrong constructor in pages.Frame.

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -1760,7 +1760,7 @@ class Frame(Wrapped):
                             domain     = g.domain))
         else:
             title = g.domain
-        Templated.__init__(self, url = url, title = title,
+        Wrapped.__init__(self, url = url, title = title,
                            fullname = fullname, thumbnail = thumbnail)
 
 class FrameToolbar(Wrapped):
