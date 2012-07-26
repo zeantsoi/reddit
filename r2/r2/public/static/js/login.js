ade8c5ff9b5020a663f52ffd8063098551f33fdd
Max Goodman <max@reddit.com>
2012-07-25 18:43:21 -0700

    Fire a pixel for UI flow tracking of sub/unsubscribe.
    This will allow us to collect information about how users found
    subreddits that they subscribe to. We can use this information to test
    the effectiveness of new ways of discovering and subscribing to
    subreddits.
    
    Specifically, when the subscribe button is clicked, for the current page
    and previous page: the URL, referrer URL, and the type of UI element
    clicked are sent. We'll use this to answer questions like:
    
     * "did clicking on gizmo A lead to users subscribing to subreddit B?"
     * "why did we see a spike in subscriptions to subreddit X today?"

--- r2/r2/public/static/js/login.js
+++ r2/r2/public/static/js/login.js
@@ -10,7 +10,7 @@ r.login = {
             endpoint = r.config.https_endpoint || ('http://'+r.config.ajax_domain),
             apiTarget = endpoint+'/api/'+action+'/'+username
 
-        if (r.utils.currentOrigin == endpoint || $.support.cors) {
+        if (r.config.currentOrigin == endpoint || $.support.cors) {
             var params = form.serialize()
             params.push({name:'api_type', value:'json'})
             $.ajax({
@@ -190,7 +190,7 @@ r.ui.LoginForm.prototype = $.extend(new r.ui.Form(), {
 
     _handleNetError: function(result, err, xhr) {
         r.ui.Form.prototype._handleNetError.apply(this, arguments)
-        if (xhr.status == 0 && r.utils.currentOrigin != r.config.https_endpoint) {
+        if (xhr.status == 0 && r.config.currentOrigin != r.config.https_endpoint) {
             $('<p>').append(
                 $('<a>')
                     .text(r.strings.login_fallback_msg)
--- r2/r2/public/static/js/utils.js
+++ r2/r2/public/static/js/utils.js
@@ -3,5 +3,30 @@ r.utils = {
         return r.config.static_root + '/' + item
     },
 
-    currentOrigin: location.protocol+'//'+location.host,
+    querySelectorFromEl: function(targetEl, selector) {
+        return $(targetEl).parents().andSelf()
+            .filter(selector || '*')
+            .map(function(idx, el) {
+                var parts = [],
+                    $el = $(el),
+                    elFullname = $el.data('fullname'),
+                    elId = $el.attr('id'),
+                    elClass = $el.attr('class')
+
+                parts.push(el.nodeName.toLowerCase())
+
+                if (elFullname) {
+                    parts.push('[data-fullname="' + elFullname + '"]')
+                } else {
+                    if (elId) {
+                        parts.push('#' + elId)
+                    } else if (elClass) {
+                        parts.push('.' + _.compact(elClass.split(/\s+/)).join('.'))
+                    }
+                }
+
+                return parts.join('')
+            })
+            .toArray().join(' ')
+    }
 }
