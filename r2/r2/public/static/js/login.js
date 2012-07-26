4e63e7348c22a2d9b4b6ece8423aa3ae2ffb144b
Max Goodman <max@reddit.com>
2012-07-25 18:41:50 -0700

    Move currentOrigin into r.utils.

--- r2/r2/public/static/js/login.js
+++ r2/r2/public/static/js/login.js
@@ -1,6 +1,4 @@
 r.login = {
-    currentOrigin: location.protocol+'//'+location.host,
-
     post: function(form, action, callback) {
         if (r.config.cnameframe && !r.config.https_endpoint) {
             form.$el.unbind()
@@ -12,7 +10,7 @@ r.login = {
             endpoint = r.config.https_endpoint || ('http://'+r.config.ajax_domain),
             apiTarget = endpoint+'/api/'+action+'/'+username
 
-        if (this.currentOrigin == endpoint || $.support.cors) {
+        if (r.utils.currentOrigin == endpoint || $.support.cors) {
             var params = form.serialize()
             params.push({name:'api_type', value:'json'})
             $.ajax({
@@ -192,7 +190,7 @@ r.ui.LoginForm.prototype = $.extend(new r.ui.Form(), {
 
     _handleNetError: function(result, err, xhr) {
         r.ui.Form.prototype._handleNetError.apply(this, arguments)
-        if (xhr.status == 0 && r.login.currentOrigin != r.config.https_endpoint) {
+        if (xhr.status == 0 && r.utils.currentOrigin != r.config.https_endpoint) {
             $('<p>').append(
                 $('<a>')
                     .text(r.strings.login_fallback_msg)
--- r2/r2/public/static/js/utils.js
+++ r2/r2/public/static/js/utils.js
@@ -1,5 +1,7 @@
 r.utils = {
     staticURL: function (item) {
         return r.config.static_root + '/' + item
-    }
+    },
+
+    currentOrigin: location.protocol+'//'+location.host,
 }
