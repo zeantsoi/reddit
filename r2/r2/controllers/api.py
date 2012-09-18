d6c12e2451e870df8a0bd21376a7136cc7c318b1
Logan Hanks <logan@reddit.com>
2012-09-18 14:28:16 -0700

    Remove oauth2 support from POST_setflairenabled.

--- r2/r2/controllers/api.py
+++ r2/r2/controllers/api.py
@@ -2486,7 +2486,6 @@ class ApiController(RedditController, OAuth2ResourceController):
 
         return BoringPage(_("API"), content = results).render()
 
-    @require_oauth2_scope("modflair")
     @validatedForm(VUser(),
                    VModhash(),
                    flair_enabled = VBoolean("flair_enabled"))
