bac38fe3a41cd09643b37bad947f3b3a70d5784d
Neil Williams <neil@reddit.com>
2012-12-27 11:20:56 -0800

    Apply a bandaid for _dirties being stuck in memcached.

--- r2/r2/lib/db/thing.py
+++ r2/r2/lib/db/thing.py
@@ -418,6 +418,9 @@ class DataThing(object):
                 bases[i] = items_db([i]).values()[0]
                 bases[i]._cache_myself()
 
+            if bases[i] and bases[i]._dirties:
+                g.log.error("Got _dirties back from the cache for %r. Cleaning up.", bases[i])
+                bases[i]._dirties.clear()
 
         if data:
             need = []
