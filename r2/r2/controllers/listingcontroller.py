d65365ac1cbfa65213680b15496c73a37e43d3bd
Chad Birch <chad.birch@gmail.com>
2013-02-22 18:21:15 -0700

    Add body classes to all remaining subreddit pages

--- r2/r2/controllers/listingcontroller.py
+++ r2/r2/controllers/listingcontroller.py
@@ -221,6 +221,7 @@ class FixListing(object):
 
 class HotController(FixListing, ListingController):
     where = 'hot'
+    extra_page_classes = ['hot-page']
 
     def spotlight(self):
         """Build the Spotlight or a single promoted link.
@@ -388,6 +389,7 @@ class HotController(FixListing, ListingController):
 class NewController(ListingController):
     where = 'new'
     title_text = _('newest submissions')
+    extra_page_classes = ['new-page']
 
     def keep_fn(self):
         def keep(item):
@@ -429,6 +431,7 @@ class NewController(ListingController):
 class RisingController(NewController):
     where = 'rising'
     title_text = _('rising submissions')
+    extra_page_classes = ['rising-page']
 
     def query(self):
         return get_rising(c.site)
@@ -468,8 +471,10 @@ class BrowseController(ListingController):
         self.sort = sort
         if sort == 'top':
             self.title_text = _('top scoring links')
+            self.extra_page_classes = ['top-page']
         elif sort == 'controversial':
             self.title_text = _('most controversial links')
+            self.extra_page_classes = ['controversial-page']
         else:
             # 'sort' is forced to top/controversial by routing.py,
             # but in case something has gone wrong...
