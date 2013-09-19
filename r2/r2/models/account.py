c9478d6cd5416d4f7c883a8a3c415729284e66a2
Neil Williams <neil@reddit.com>
2013-09-19 11:20:06 -0700

    Add default for new cake system.

--- r2/r2/models/account.py
+++ r2/r2/models/account.py
@@ -113,6 +113,7 @@ class Account(Thing):
                      gold_charter = False,
                      gold_creddits = 0,
                      gold_creddit_escrow = 0,
+                     cake_expiration=None,
                      otp_secret=None,
                      state=0,
                      )
