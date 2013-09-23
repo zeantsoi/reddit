88486a8d5dade56394f210973163a44414425028
Neil Williams <neil@reddit.com>
2013-09-23 11:00:31 -0700

    Revert "Classify the landlubber cookie for stats."
    This reverts commit 75ad5b348af4572ba590a13e1f63c2de974c8dae.

--- r2/r2/controllers/reddit_base.py
+++ r2/r2/controllers/reddit_base.py
@@ -177,8 +177,6 @@ class Cookie(object):
             return "ga"
         elif cookie_name.startswith("beta_"):
             return "beta"
-        elif cookie_name.startswith("landlubber"):
-            return "landlubber"
         else:
             return "other"
 
