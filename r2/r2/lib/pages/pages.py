677bd96097b402d116a471109d581b1295a6b220
Max Goodman <c@chromakode.com>
2014-04-24 18:37:34 -0700

    Add /explore variant to next/prev suggestions.

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -4348,8 +4348,14 @@ class SubredditSelector(Templated):
 
 class ListingSuggestions(Templated):
     def __init__(self):
+        Templated.__init__(self)
+
         self.suggestion_type = None
         if c.default_sr:
+            if c.user_is_loggedin and random.randint(0, 1) == 1:
+                self.suggestion_type = "explore"
+                return
+
             multis = c.user_is_loggedin and LabeledMulti.by_owner(c.user)
 
             if multis and c.site in multis:
@@ -4363,8 +4369,6 @@ class ListingSuggestions(Templated):
                     self.suggestions = random.sample(multis, 3)
             else:
                 self.suggestion_type = "random"
-
-        Templated.__init__(self)
 
 
 class ExploreItem(Templated):
