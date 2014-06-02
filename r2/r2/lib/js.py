ef56de8336a96fa72fd9ffadb40253f1cf4f07f8
Jack Lawson <ajacksified@gmail.com>
2014-06-02 14:56:31 -0700

    Log browser performance data to statsd.

--- r2/r2/lib/js.py
+++ r2/r2/lib/js.py
@@ -469,6 +469,7 @@ module["reddit"] = LocalizedModule("reddit.js",
     "lib/jquery.cookie.js",
     "lib/jquery.url.js",
     "lib/backbone-1.0.0.js",
+    "timings.js",
     "templates.js",
     "scrollupdater.js",
     "timetext.js",
@@ -492,8 +493,9 @@ module["reddit"] = LocalizedModule("reddit.js",
 )
 
 module["admin"] = Module("admin.js",
-    # include Backbone so it is available early to render admin bar fast.
+    # include Backbone and timings so they are available early to render admin bar fast.
     "lib/backbone-1.0.0.js",
+    "timings.js",
     "adminbar.js",
 )
 
