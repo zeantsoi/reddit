124f0132fb70d71ed5cd51fe437e417c0baaa00f
umbrae <umbrae@reddit.com>
2014-06-26 19:59:44 -0700

    Add controversiality to comment JSON
    As an integer for future proofing if we eventually provide levels of controversiality.

--- r2/r2/lib/jsontemplates.py
+++ r2/r2/lib/jsontemplates.py
@@ -503,6 +503,7 @@ class CommentJsonTemplate(ThingJsonTemplate):
         body="body",
         body_html="body_html",
         distinguished="distinguished",
+        controversiality="controversiality",
         downs="downvotes",
         edited="editted",
         gilded="gilded",
@@ -523,6 +524,8 @@ class CommentJsonTemplate(ThingJsonTemplate):
         from r2.models import Comment, Link, Subreddit
         if attr == 'link_id':
             return make_fullname(Link, thing.link_id)
+        elif attr == "controversiality":
+            return 1 if thing.is_controversial else 0
         elif attr == "editted" and not isinstance(thing.editted, bool):
             return (time.mktime(thing.editted.astimezone(pytz.UTC).timetuple())
                     - time.timezone)
@@ -539,6 +542,7 @@ class CommentJsonTemplate(ThingJsonTemplate):
             return spaceCompress(safemarkdown(thing.body))
         elif attr == "gilded":
             return thing.gildings
+
         return ThingJsonTemplate.thing_attr(self, thing, attr)
 
     def kind(self, wrapped):
