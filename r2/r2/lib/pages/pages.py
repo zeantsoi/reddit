75a06e7ea5917640a8fe6f440109b0c8fdf1bdc6
Matt Lee <matt@reddit.com>
2014-07-31 14:05:57 -0700

    beta: Redesign for edit_promo/new_promo pages.

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -3587,6 +3587,11 @@ class PromotePage(Reddit):
     searchbox          = False
 
     def __init__(self, nav_menus = None, *a, **kw):
+        extra_page_classes = []
+        if feature.is_enabled('promote_redesign'):
+            extra_page_classes.append('adbeta')
+        else:
+            extra_page_classes.append('no-adbeta')
         buttons = [NamedButton('new_promo')]
         if c.user_is_sponsor:
             buttons.append(NamedButton('roadblock'))
@@ -3610,7 +3615,8 @@ class PromotePage(Reddit):
             nav_menus = [menu]
 
         kw['show_sidebar'] = False
-        Reddit.__init__(self, nav_menus = nav_menus, *a, **kw)
+        Reddit.__init__(self, nav_menus=nav_menus,
+                        extra_page_classes=extra_page_classes, *a, **kw)
 
 class PromoteLinkNew(Templated):
     pass
