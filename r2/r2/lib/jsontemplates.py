24e89e9f292ae3de225746097f9b3c1fa9bffd52
Brian Simpson <bsimpson63@gmail.com>
2014-09-06 22:28:01 -0400

    Revert "ThingJsonTemplate: inspect WrappedUser to get flair text and css."
    This reverts commit f07105a882665118ae2071098b0ca3aea4e98771.

--- r2/r2/lib/jsontemplates.py
+++ r2/r2/lib/jsontemplates.py
@@ -136,13 +136,23 @@ class ThingJsonTemplate(JsonTemplate):
                 return "[deleted]"
             return thing.author.name
         if attr == "author_flair_text":
-            if thing.author._deleted or not thing.wrapped_author.flair_enabled:
+            if thing.author._deleted:
                 return None
-            return thing.wrapped_author.flair_text
+            if thing.author.flair_enabled_in_sr(thing.subreddit._id):
+                return getattr(thing.author,
+                               'flair_%s_text' % (thing.subreddit._id),
+                               None)
+            else:
+                return None
         if attr == "author_flair_css_class":
-            if thing.author._deleted or not thing.wrapped_author.flair_enabled:
+            if thing.author._deleted:
                 return None
-            return thing.wrapped_author.flair_css_class
+            if thing.author.flair_enabled_in_sr(thing.subreddit._id):
+                return getattr(thing.author,
+                               'flair_%s_css_class' % (thing.subreddit._id),
+                               None)
+            else:
+                return None
         elif attr == "created":
             return time.mktime(thing._date.timetuple())
         elif attr == "created_utc":
