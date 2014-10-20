zeantsoi/reddit

commit 80530b6637f2b8d10a26c297e67b59aebb0a875d

Add jquery/html5shim directly to bundle.
Also removes "load core JS libraries from reddit servers" as
a preference since it is no longer needed.

Using the google cdn for jquery adds very little benefit, see:
http://statichtml.com/2011/google-ajax-libraries-caching.html
and
https://github.com/h5bp/html5-boilerplate/pull/1327

r2/r2/lib/js.py
@@ -393,42 +393,12 @@ class LocalizedModule(Module):
         for lang, unused in iter_langs():
             yield LocalizedModule.languagize_path(self.path, lang)
 
-
-class JQuery(Module):
-    versions = {
-        1: "1.11.1",
-        2: "2.1.1",
-    }
-
-    def __init__(self, cdn_url="http://ajax.googleapis.com/ajax/libs/jquery/{version}/jquery", major_version=1):
-        self.jquery_src = FileSource("lib/jquery-{0}.min.js".format(self.versions[major_version]))
-        Module.__init__(self, "jquery-{0}.min.js".format(self.versions[major_version]), self.jquery_src, should_compile=False)
-        self.cdn_src = cdn_url.format(version=self.versions[major_version])
-
-    def use(self):
-        from r2.lib.template_helpers import static
-        if c.secure or (c.user and c.user.pref_local_js):
-            return Module.use(self)
-        else:
-            ext = ".js" if g.uncompressedJS else ".min.js"
-            return script_tag.format(src=self.cdn_src+ext)
-
-
 module = {}
-
-
-module["jquery1x"] = JQuery(major_version=1)
-module["jquery2x"] = JQuery(major_version=2)
-
-
-module["html5shiv"] = Module("html5shiv.js",
-    "lib/html5shiv.js",
-    should_compile=False
-)
 
 catch_errors = "try {{ {content} }} catch (err) {{ r.sendError('Error running module', '{name}', ':', err) }}"
 
-module["reddit-init"] = LocalizedModule("reddit-init.js",
+
+module["reddit-init-base"] = LocalizedModule("reddit-init-base.js",
     "lib/es5-shim.js",
     "lib/json2.js",
     "lib/underscore-1.4.4.js",
@@ -450,6 +420,17 @@ module["reddit-init"] = LocalizedModule("reddit-init.js",
         PluralForms(),
     ],
     wrap=catch_errors,
+)
+
+module["reddit-init-legacy"] = LocalizedModule("reddit-init-legacy.js",
+    "lib/html5shiv.js",
+    "lib/jquery-1.11.1.js",
+    module["reddit-init-base"],
+)
+
+module["reddit-init"] = LocalizedModule("reddit-init.js",
+    "lib/jquery-2.1.1.js",
+    module["reddit-init-base"],
 )
 
 module["reddit"] = LocalizedModule("reddit.js",

r2/r2/lib/validator/preferences.py
@@ -62,7 +62,6 @@ PREFS_VALIDATORS = dict(
     pref_threaded_messages=VBoolean("threaded_messages"),
     pref_collapse_read_messages=VBoolean("collapse_read_messages"),
     pref_private_feeds=VBoolean("private_feeds"),
-    pref_local_js=VBoolean('local_js'),
     pref_store_visits=VBoolean('store_visits'),
     pref_show_adbox=VBoolean("show_adbox"),
     pref_show_sponsors=VBoolean("show_sponsors"),

r2/r2/models/account.py
@@ -95,7 +95,6 @@ class Account(Thing):
                      pref_threaded_messages = True,
                      pref_collapse_read_messages = False,
                      pref_private_feeds = True,
-                     pref_local_js = False,
                      pref_force_https = False,
                      pref_show_adbox = True,
                      pref_show_sponsors = True, # sponsored links

r2/r2/templates/base.compact
@@ -47,11 +47,11 @@
     <link rel="stylesheet" href="${static('compact.css')}" type="text/css" media="screen" />
 
     <!--[if gte IE 9]> <!-->
-      ${unsafe(js.use('jquery2x', 'reddit-init'))}
+      ${unsafe(js.use('reddit-init'))}
     <!-- <![endif]-->
 
     <!--[if lt IE 9]>
-      ${unsafe(js.use('jquery1x', 'reddit-init'))}
+      ${unsafe(js.use('reddit-init-legacy'))}
     <![endif]-->
 
     ${js_setup(thing.extra_js_config)}

r2/r2/templates/prefoptions.html
@@ -296,16 +296,6 @@
         <a href="http://www.reddit.com/wiki/noindex">${_("details")}</a>
         )
       </span>
-      <br />
-      ${checkbox(_("load core JS libraries from reddit servers"), "local_js")}
-      &#32;
-      <span class="little gray">
-       (
-         <a href="http://www.reddit.com/wiki/localjs">
-           ${_("details")}
-         </a>
-       )
-      </span>
     </td>
   </tr>
   %endif

r2/r2/templates/reddit.html
@@ -114,11 +114,11 @@
 <%def name="javascript()">
   <% from r2.lib import js %>
   <!--[if gte IE 9]> <!-->
-    ${unsafe(js.use('jquery2x', 'reddit-init'))}
+    ${unsafe(js.use('reddit-init'))}
   <!-- <![endif]-->
 
   <!--[if lt IE 9]>
-    ${unsafe(js.use('html5shiv', 'jquery1x', 'reddit-init'))}
+    ${unsafe(js.use('reddit-init-legacy'))}
   <![endif]-->
   ${less_js()}
 </%def>

commit f34871e786c15d719c63c613062051da0dc1da66

Fix framebuster/mobile toolbar with new bundles
This fixes a regression introduced with
80530b6637f2b8d10a26c297e67b59aebb0a875d.

r2/r2/lib/js.py
@@ -463,6 +463,16 @@ module["reddit"] = LocalizedModule("reddit.js",
     wrap=catch_errors,
 )
 
+module["framebuster"] = LocalizedModule("framebuster.js",
+    "lib/jquery-2.1.1.js",
+    module["reddit"],
+)
+
+module["framebuster-legacy"] = LocalizedModule("framebuster-legacy.js",
+    "lib/jquery-1.11.1.js",
+    module["reddit"],
+)
+
 module["admin"] = Module("admin.js",
     # include Backbone and timings so they are available early to render admin bar fast.
     "lib/backbone-1.0.0.js",

r2/r2/templates/frame.compact
@@ -33,11 +33,11 @@
     <title>${thing.title}</title>
 
     <!--[if gte IE 9]> <!-->
-      ${unsafe(js.use('jquery2x', 'mobile'))}
+      ${unsafe(js.use('reddit-init', 'mobile'))}
     <!-- <![endif]-->
 
     <!--[if lt IE 9]>
-      ${unsafe(js.use('jquery1x', 'mobile'))}
+      ${unsafe(js.use('reddit-init-legacy', 'mobile'))}
     <![endif]-->
     ${js_setup()}
     <link rel="stylesheet" href="${static('compact.css')}"

r2/r2/templates/framebuster.html
@@ -30,11 +30,11 @@
 
 
     <!--[if gte IE 9]> <!-->
-      ${unsafe(js.use('jquery2x', 'reddit'))}
+      ${unsafe(js.use('framebuster'))}
     <!-- <![endif]-->
 
     <!--[if lt IE 9]>
-      ${unsafe(js.use('jquery1x', 'reddit'))}
+      ${unsafe(js.use('framebuster-legacy'))}
     <![endif]-->
     ${js_setup()}
   </head>