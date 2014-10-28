02e498cbeed9e1e82e497c5b74a5d722a40112a2
Robert Ditthardt <drew.ditthardt@gmail.com>
2014-10-28 10:53:35 -0700

    Relation: Stop dual writing keys.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -240,7 +240,6 @@ class Globals(object):
             'pagecaches',
             'memoizecaches',
             'srmembercaches',
-            'srmember2caches',
             'relcaches',
             'ratelimitcaches',
             'cassandra_seeds',
@@ -580,14 +579,6 @@ class Globals(object):
             binary=True,
         )
 
-        # temporary transition pool
-        srmember2caches = CMemcache(
-            self.srmember2caches,
-            min_compress_len=96,
-            num_clients=num_mc_clients,
-            binary=True,
-        )
-
         # a pool just for rels
         relcaches = CMemcache(
             self.relcaches,
@@ -712,18 +703,10 @@ class Globals(object):
                 stalecaches,
                 srmembercaches,
             )
-            self.srmember2cache = StaleCacheChain(
-                localcache_cls(),
-                stalecaches,
-                srmember2caches,
-            )
         else:
             self.srmembercache = MemcacheChain(
                 (localcache_cls(), srmembercaches))
-            self.srmember2cache = MemcacheChain(
-                (localcache_cls(), srmember2caches))
         cache_chains.update(srmembercache=self.srmembercache)
-        cache_chains.update(srmember2cache=self.srmember2cache)
 
         if stalecaches:
             self.relcache = StaleCacheChain(
