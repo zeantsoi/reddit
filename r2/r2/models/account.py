5c0d5d0894400b3803faa19ef881a07a659d383c
MelissaCole <melissa.engineer@gmail.com>
2015-01-06 12:28:00 -0800

    Revert "Add snoovatar link on userpage of those with public snoovatar and gold."
    This reverts commit c53d90eeaff7d1d0ec8d43efd91cd6a09ed7f25d.

--- r2/r2/models/account.py
+++ r2/r2/models/account.py
@@ -136,7 +136,6 @@ class Account(Thing):
                      update_sent_messages=True,
                      num_payment_methods=0,
                      num_failed_payments=0,
-                     pref_show_snoovatar=False,
                      )
     _preference_attrs = tuple(k for k in _defaults.keys()
                               if k.startswith("pref_"))
