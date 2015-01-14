537944dcf78477d9dd6cfec718cf58c61b287b13
Brian Simpson <bsimpson63@gmail.com>
2015-01-14 14:36:35 -0500

    Cut over reads to new main memcache pool.
    For g.cache this means flipping the `read_original` flags to False.
    
    For g.hardcache this means moving memcache_m3 earlier in the
    cache chain than memcache_m1.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -727,7 +727,7 @@ class Globals(object):
             self.cache = TransitionalCache(
                     original_cache=stale_m1,
                     replacement_cache=stale_m3,
-                    read_original=True,
+                    read_original=False,
             )
         else:
             simple_m1 = CacheChain((localcache_cls(), memcache_m1))
@@ -735,7 +735,7 @@ class Globals(object):
             self.cache = TransitionalCache(
                     original_cache=simple_m1,
                     replacement_cache=simple_m3,
-                    read_original=True,
+                    read_original=False,
             )
         cache_chains.update(cache=self.cache)
 
@@ -803,7 +803,7 @@ class Globals(object):
         # hardcache is used for various things that tend to expire
         # TODO: replace hardcache w/ cassandra stuff
         self.hardcache = HardcacheChain(
-            (localcache_cls(), memcache_m1, memcache_m3, HardCache(self)),
+            (localcache_cls(), memcache_m3, memcache_m1, HardCache(self)),
             cache_negative_results=True,
         )
         cache_chains.update(hardcache=self.hardcache)
