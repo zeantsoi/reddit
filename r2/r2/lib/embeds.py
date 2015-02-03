0cd7b7eb293b177e6d8ee9b2fabc3ad3702220e3
umbrae <umbrae@reddit.com>
2015-02-02 22:06:10 -0800

    Revert "Split embed preparation into two stages to allow framing on 304s"
    This reverts commit 9f9bbe61fe2e3fea2c8a31361ddc3f6510c694b8.

--- r2/r2/lib/embeds.py
+++ r2/r2/lib/embeds.py
@@ -16,13 +16,10 @@ DISALLOWED_SR_TYPES = {"private", "gold_restricted"}
 
 
 def embeddable_sr(thing):
-    if isinstance(thing, Subreddit):
-        sr = thing
-    else:
-        try:
-            sr = Subreddit._byID(thing.sr_id) if thing.sr_id else None
-        except NotFound:
-            sr = None
+    try:
+        sr = Subreddit._byID(thing.sr_id) if thing.sr_id else None
+    except NotFound:
+        sr = None
 
     return sr if (sr is not None and sr.type not in DISALLOWED_SR_TYPES) else False
 
@@ -44,60 +41,49 @@ def edited_after(thing, iso_timestamp, showedits):
     return created < thing.editted
 
 
-def prepare_embed_request(sr):
-    """Given a request, determine if we are embedding. If so, ensure the
-       subreddit is embeddable, prepare the request for embedding, and return
-       the key.
-    """
+def setup_embed(thing, showedits):
     embed_key = request.GET.get('embed')
+    if embed_key:
+        if request.host != g.media_domain:
+            # don't serve up untrusted content except on our
+            # specifically untrusted domain
+            abort(404)
 
-    if not embed_key:
-        return None
+        sr = embeddable_sr(thing)
+        if not sr:
+            abort(404)
 
-    if request.host != g.media_domain:
-        # don't serve up untrusted content except on our
-        # specifically untrusted domain
-        abort(404)
+        expected_mac = hmac.new(g.secrets['comment_embed'], thing._id36,
+                                hashlib.sha1).hexdigest()
+        if not constant_time_compare(embed_key or '', expected_mac):
+            abort(401)
 
-    if not embeddable_sr(sr):
-        abort(404)
+        try:
+            author = Account._byID(thing.author_id) if thing.author_id else None
+        except NotFound:
+            author = None
 
-    c.allow_framing = True
+        iso_timestamp = request.GET.get("created", "")
 
-    return embed_key
+        c.embed_config = {
+            "logged": c.user_is_loggedin,
+            "stats_domain": g.stats_domain or "",
+            "created": iso_timestamp,
+            "showedits": showedits,
+            "thing": {
+                "id": thing._id,
+                "sr_id": sr._id,
+                "sr_name": sr.name,
+                "edited": edited_after(thing, iso_timestamp, showedits),
+                "deleted": thing.deleted or author._deleted,
+            },
+        }
 
-
-def set_up_embed(embed_key, sr, thing, showedits):
-    expected_mac = hmac.new(g.secrets['comment_embed'], thing._id36,
-                            hashlib.sha1).hexdigest()
-    if not constant_time_compare(embed_key or '', expected_mac):
-        abort(401)
-
-    try:
-        author = Account._byID(thing.author_id) if thing.author_id else None
-    except NotFound:
-        author = None
-
-    iso_timestamp = request.GET.get("created", "")
-
-    c.embed_config = {
-        "logged": c.user_is_loggedin,
-        "stats_domain": g.stats_domain or "",
-        "created": iso_timestamp,
-        "showedits": showedits,
-        "thing": {
-            "id": thing._id,
-            "sr_id": sr._id,
-            "sr_name": sr.name,
-            "edited": edited_after(thing, iso_timestamp, showedits),
-            "deleted": thing.deleted or author._deleted,
-        },
-    }
-
-    c.render_style = "iframe"
-    c.user = UnloggedUser([c.lang])
-    c.user_is_loggedin = False
-    c.forced_loggedout = True
+        c.render_style = "iframe"
+        c.user = UnloggedUser([c.lang])
+        c.user_is_loggedin = False
+        c.forced_loggedout = True
+        c.allow_framing = True
 
 
 def is_embed():
