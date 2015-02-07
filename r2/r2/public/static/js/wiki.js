f30c8bdb767e57e37ffe17a36f77ec28d47955d5
Chad Birch <chad.birch@gmail.com>
2015-02-06 19:56:33 -0700

    Integrate AutoModerator into the site

--- r2/r2/public/static/js/wiki.js
+++ r2/r2/public/static/js/wiki.js
@@ -148,7 +148,7 @@ r.wiki = {
                         ,specials = special.children('#specials')
                     specials.empty()
                     for(i in errors) {
-                        specials.append($('<p>').text(errors[i]))
+                        specials.append($('<pre>').text(errors[i]))
                     }
                     special.fadeIn('slow')
                 },
