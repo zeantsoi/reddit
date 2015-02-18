5b0d1608dc7175562098f8666ea2acb39f829d9a
Avinash Dwarapu <avidw@users.noreply.github.com>
2015-02-18 00:42:27 +0000

    Change /{article}/related to /related/{article} in API docs
    Frankly surprised this error hasn't been caught...

--- r2/r2/controllers/front.py
+++ r2/r2/controllers/front.py
@@ -830,7 +830,7 @@ class FrontController(RedditController):
     @base_listing
     @require_oauth2_scope("read")
     @validate(article=VLink('article'))
-    @api_doc(api_section.listings, uri="/{article}/related")
+    @api_doc(api_section.listings, uri="/related/{article}")
     def GET_related(self, num, article, after, reverse, count):
         """Related page: performs a search using title of article as
         the search query.
