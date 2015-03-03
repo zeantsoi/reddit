cae9d68e87c13309b0eb60cedc75adcd76257919
Jordan Milne <jordan.milne@saynotolinux.com>
2015-03-02 19:59:32 -0800

    Add notes and debug output to deleted account backunfill script
    Lest others repeat our mistakes

--- scripts/migrate/backfill/scrub_deleted_users.py
+++ scripts/migrate/backfill/scrub_deleted_users.py
@@ -20,7 +20,17 @@
 # Inc. All Rights Reserved.
 ###############################################################################
 
+"""
+Script for backunfilling data from deleted users.
+
+You might want to change `run_changed()` to `run_changed(use_safe_get=True)`
+in `reddit-consumer-cloudsearch_q.conf` unless you're sure *everything* in
+`LinksByAccount` is a valid `Link`. Otherwise, you're gonna back up the
+cloudsearch queue.
+"""
+
 import time
+import sys
 
 from r2.lib.db.operators import desc
 from r2.lib.utils import fetch_things2, progress
@@ -41,9 +51,15 @@ def backfill_deleted_accounts(resume_id=None):
         del_accts._filter(Account.c._id < resume_id)
 
     for i, account in enumerate(progress(fetch_things2(del_accts))):
-        # Don't kill the rabbit!
+        # Don't kill the rabbit! Wait for the relevant queues to calm down.
         if i % 1000 == 0:
-            while get_queue_length('del_account_q') > 10000:
+            del_len = get_queue_length('del_account_q')
+            cs_len = get_queue_length('cloudsearch_changes')
+            while (del_len > 1000 or
+                    cs_len > 10000):
+                sys.stderr.write(("CS: %d, DEL: %d" % (cs_len, del_len)) + "\n")
+                sys.stderr.flush()
                 time.sleep(1)
+                del_len = get_queue_length('del_account_q')
+                cs_len = get_queue_length('cloudsearch_changes')
         amqp.add_item('account_deleted', account._fullname)
-
