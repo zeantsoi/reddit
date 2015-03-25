7338cd064facadc6788517c7d313ba3160c5b1b9
zeantsoi <zeantsoi@gmail.com>
2015-03-25 11:40:15 -0700

    Add ability for sponsors to add URL to be inserted into impression tracking pixel

--- r2/r2/public/static/js/analytics.js
+++ r2/r2/public/static/js/analytics.js
@@ -158,18 +158,25 @@ r.analytics = {
       return;
     }
 
+    var adBlockIsEnabled = $('#siteTable_organic').is(":hidden");
     var pixel = new Image();
     var impPixel = $el.data('impPixel');
 
-    if (impPixel) {
+    if (impPixel && !adBlockIsEnabled) {
       pixel.src = impPixel;
+    }
+
+    var thirdPartyTrackingUrl = $el.data('thirdPartyTrackingUrl');
+    if (thirdPartyTrackingUrl && !adBlockIsEnabled) {
+      var thirdPartyTrackingImage = new Image();
+      thirdPartyTrackingImage.src = thirdPartyTrackingUrl;
     }
 
     var adServerPixel = new Image();
     var adServerImpPixel = $el.data('adserverImpPixel');
     var adServerClickUrl = $el.data('adserverClickUrl');
 
-    if (adServerImpPixel) {
+    if (adServerImpPixel && !adBlockIsEnabled) {
       adServerPixel.src = adServerImpPixel;
     }
 
