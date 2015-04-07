3989aa7f33229feed6a35c3aa172aea743e8410b
Neil Williams <neil@reddit.com>
2015-04-06 21:49:07 -0700

    New comment highlighting: Remove server-side pv_hex stuff.
    Previously, the server would check the user's previous visits when
    redering a comment page and add comment-period-N classes to comments
    depending on where they fell in relation to those visits.  The client
    side would then add or remove a new-comment class to every comment with
    the appropriate (or older) comment-period class on first load or when
    the previous visit selection changed.
    
    This removes that server-side addition of comment-period-N classes and
    replaces it with ScrollUpdater-based updating of comments based on their
    actual timestamps. The goal is to reduce some server-side ugliness and
    extraneous memcached lookups.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -1354,7 +1354,7 @@ class Comment(Thing, Printable):
 
             item.editted = getattr(item, "editted", False)
 
-            item.render_css_class = "comment %s" % CachedVariable("time_period")
+            item.render_css_class = "comment"
 
             #will get updated in builder
             item.num_children = 0
@@ -1491,8 +1491,6 @@ class MoreComments(Printable):
         return False
 
     def __init__(self, link, depth, parent_id=None):
-        from r2.lib.wrapped import CachedVariable
-
         if parent_id is not None:
             id36 = utils.to36(parent_id)
             self.parent_id = parent_id
@@ -1503,7 +1501,6 @@ class MoreComments(Printable):
         self.depth = depth
         self.children = []
         self.count = 0
-        self.previous_visits_hex = CachedVariable("previous_visits_hex")
 
     @property
     def _fullname(self):
