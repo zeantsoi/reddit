260ad1d93c32480154cea5c4026e5f4793a02403
Keith Mitchell <kemitche@reddit.com>
2015-04-30 11:41:52 -0700

    Revert "event-collector: Don't clobber uuid module"
    This reverts commit f1554ca38d37a2330474f5287ed39f343c3c59bb.

--- r2/r2/lib/eventcollector.py
+++ r2/r2/lib/eventcollector.py
@@ -128,10 +128,10 @@ class Event(dict):
     @classmethod
     def base(cls, event_name=None, timestamp=None, user_agent=None, ip=None,
               domain=None, user_id=None, loid=None, oauth2_client_id=None,
-              event_uuid=None, **kw):
+              uuid=None, **kw):
         ret = cls(kw)
 
-        if event_uuid is None:
+        if uuid is None:
             ret["uuid"] = uuid.uuid4()
 
         if event_name is not None:
