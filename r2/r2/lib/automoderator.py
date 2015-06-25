98227759c5eec403c9abb9287a78a95007a8c29c
Chad Birch <chad.birch@gmail.com>
2015-06-24 20:31:20 -0700

    AutoMod: send messages from the subreddit
    This will allow users to reply directly to the AutoModerator message in
    order to contact the mods, and should make it more clear what the source
    of the message was.

--- r2/r2/lib/automoderator.py
+++ r2/r2/lib/automoderator.py
@@ -1423,8 +1423,8 @@ class Rule(object):
             subject = subject[:100]
 
             new_message, inbox_rel = Message._new(ACCOUNT, data["author"],
-                subject, message, None)
-            queries.new_message(new_message, inbox_rel)
+                subject, message, None, sr=data["subreddit"], from_sr=True)
+            queries.new_message(new_message, inbox_rel, update_modmail=False)
 
             g.stats.simple_event("automoderator.message")
 
