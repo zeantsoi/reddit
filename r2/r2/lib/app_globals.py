44248433712bad4884a7772588a8677cb188a3f9
zeantsoi <zeantsoi@gmail.com>
2015-06-25 20:43:20 -0700

    Enable validation of CVV

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -240,6 +240,7 @@ class Globals(object):
             'trust_local_proxies',
             'shard_link_vote_queues',
             'shard_commentstree_queues',
+            'authnet_validate',
             'ENFORCE_RATELIMIT',
             'RL_SITEWIDE_ENABLED',
             'RL_OAUTH_SITEWIDE_ENABLED',
