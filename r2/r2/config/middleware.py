0b903ae60b829defff94afac1e851bb877fc427b
Brian Simpson <bsimpson63@gmail.com>
2015-07-24 03:27:43 -0400

    Revert "Tests: Refactor test initialization"
    This reverts commit 6c8999d4ba8fc0d494428e6cc4e8c3f14aaf13ec.
    
    That commit was preventing plugin hooks from registering in
    reddit-run and reddit-shell contexts.

--- r2/r2/config/middleware.py
+++ r2/r2/config/middleware.py
@@ -437,8 +437,6 @@ class RedditApp(PylonsApp):
 
     def setup_app_env(self, environ, start_response):
         PylonsApp.setup_app_env(self, environ, start_response)
-        if environ.get('paste.testing'):
-            return
         self.load()
 
     def load(self):
