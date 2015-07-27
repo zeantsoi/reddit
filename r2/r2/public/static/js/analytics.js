c5c6c84ddf996892b3daba8925fb473b05ce025d
Matt Lee <matt@reddit.com>
2015-07-27 12:00:56 -0700

    Fire page tracking pixel before ref tags are stripped.

--- r2/r2/public/static/js/analytics.js
+++ r2/r2/public/static/js/analytics.js
@@ -21,7 +21,23 @@ r.analytics = {
       r.analytics.fireFunnelEvent('ads', r.config.ads_virtual_page);
     }
 
-    r.analytics.stripAnalyticsParams();
+    var url = r.config.tracker_url;
+    var params = {};
+
+    if (!r.config.user_id) {
+      var tracker = new redditlib.Tracker();
+      var loggedOutData = tracker.getTrackingData();
+      if (loggedOutData && loggedOutData.loid) {
+        params = {
+            loid: loggedOutData.loid
+        };
+        if (loggedOutData.loidcreated) {
+          params['loidcreated'] = loggedOutData.loidcreated
+        }
+      }
+    }
+
+    r.analytics.firePageTrackingPixel(url, params, r.analytics.stripAnalyticsParams);
   },
 
   _eventPredicates: {},
@@ -211,7 +227,7 @@ r.analytics = {
     );
   },
 
-  firePageTrackingPixel: function(url, params) {
+  firePageTrackingPixel: function(url, params, callback) {
     if (!url) { return; }
 
     params = params || {};
@@ -236,6 +252,7 @@ r.analytics = {
     }
 
     var pixel = new Image();
+    pixel.onload = pixel.onerror = callback;
     pixel.src = url + '&' + querystring.join('&');
   },
 
