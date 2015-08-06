8a7d44b23209eed45d9814bd9bac266275356f3b
David Wick <wick.d.spencer@gmail.com>
2015-08-06 12:20:01 -0700

    Remove the ability to edit the startdate of a live campaign

--- r2/r2/controllers/api.py
+++ r2/r2/controllers/api.py
@@ -4521,6 +4521,7 @@ class ApiController(RedditController):
                                   skip=True)
         listing = LinkListing(builder, nextprev=False).listing()
         promote.add_trackers(listing.things, c.site)
+        promote.update_served(listing.things)
         if listing.things:
             w = listing.things[0]
             w.num = ""
