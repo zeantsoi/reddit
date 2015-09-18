d9306ae6d4abee64c09679fcdbbeb8551c53d9a8
xiongchiamiov <xiong.chiamiov@gmail.com>
2015-09-18 12:32:44 -0700

    Remove text on user-deleted self-posts
    When a user deletes their comment, the text is removed.  Now self-posts work
    similarly, so we're consistent and less surprising (especially since after
    deletion the user can no longer edit the text out).

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -745,6 +745,7 @@ class Link(Thing, Printable):
             if item.deleted and not c.user_is_admin:
                 item.author = DeletedUser()
                 item.as_deleted = True
+                item.selftext = '[deleted]'
 
             item.votable = not item.archived
 
