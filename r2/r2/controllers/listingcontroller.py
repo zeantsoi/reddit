555f039d840b7f57567d055565a41e795faa93dd
MelissaCole <melissa.engineer@gmail.com>
2015-09-28 16:53:47 -0700

    Remove feature flag for modmail muting

--- r2/r2/controllers/listingcontroller.py
+++ r2/r2/controllers/listingcontroller.py
@@ -1745,8 +1745,6 @@ class UserListListingController(ListingController):
         elif where == 'muted':
             if not has_mod_access:
                 abort(403)
-            if not feature.is_enabled('modmail_muting'):
-                abort(403)
             self.listing_cls = MutedListing
 
         elif where == 'wikibanned':
