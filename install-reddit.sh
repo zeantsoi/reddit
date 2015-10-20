3725c69c12c5a88642fce33e23a7f9cbe696569f
Neil Williams <neil@reddit.com>
2015-10-20 13:54:54 -0700

    install-reddit: Use a single app to init the DBs before going live.
    Previously, we'd end up in a situation where all the processes starting
    up at the same time caused a big mess and failed more often than not.
    This should be a lot cleaner, if a tiny bit slower.

--- install-reddit.sh
+++ install-reddit.sh
@@ -736,6 +736,16 @@ set_consumer_count butler_q 1
 
 chown -R $REDDIT_USER:$REDDIT_GROUP $CONSUMER_CONFIG_ROOT/
 
+
+###############################################################################
+# Start everything up
+###############################################################################
+
+# the initial database setup should be done by one process rather than a bunch
+# vying with eachother to get there first
+reddit-run -c 'print "ok done"'
+
+# ok, now start everything else up
 initctl emit reddit-stop
 initctl emit reddit-start
 
