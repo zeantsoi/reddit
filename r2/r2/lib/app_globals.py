2148b3efcdcc9cd5ed7bf1b55eea888437c84855
Brian Simpson <bsimpson63@gmail.com>
2015-10-28 08:13:27 -0700

    CMemcache: remove nonfunctional `noreply` option.
    The no reply behavior isn't supported in pylibmc.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -749,7 +749,6 @@ class Globals(object):
         rendercaches = CMemcache(
             "render",
             self.rendercaches,
-            noreply=True,
             no_block=True,
             num_clients=num_mc_clients,
             min_compress_len=480,
@@ -760,7 +759,6 @@ class Globals(object):
         pagecaches = CMemcache(
             "page",
             self.pagecaches,
-            noreply=True,
             no_block=True,
             num_clients=num_mc_clients,
             min_compress_len=1400,
