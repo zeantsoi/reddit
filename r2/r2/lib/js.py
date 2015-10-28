zeantsoi/reddit

commit 5c3f569c9000e753a110a4dce26bd0d44a328e2d

Refactor JS vote function

r2/r2/lib/js.py
@@ -440,6 +440,7 @@ module["reddit-init-base"] = LocalizedModule("reddit-init-base.js",
     "lib/jquery.cookie.js",
     "bootstrap.tooltip.extension.js",
     "base.js",
+    "voting.js",
     "preload.js",
     "logging.js",
     "client-error-logger.js",

r2/r2/public/static/js/voting.js
@@ -1,0 +1,66 @@
+!function(r) {
+  var UP_CLS = "up";
+  var DOWN_CLS = "down";
+  var MouseEvent = window.MouseEvent;
+  var createEvent = document.createEvent.bind(document);
+  var theFakeClick;
+
+  if (MouseEvent) {
+    theFakeClick = new MouseEvent('click', {bubbles: true});
+  } else if (createEvent) {
+    theFakeClick = createEvent('MouseEvent');
+  } else {
+    theFakeClick = {};
+  }
+
+  window.MouseEvent = function(type, init) {
+    return theFakeClick;
+  }
+
+  document.createEvent = function(type) {
+    if (type === 'MouseEvent' || type === 'MouseEvents') {
+      return theFakeClick;
+    } else {
+      return createEvent(type);
+    }
+  }
+
+  $(function() {
+    $(document.body).on('click', '.arrow', function vote(e) {
+      var $el = $(this);
+
+      if (!r.config.logged) {
+        return;
+      }
+
+      if ($el.hasClass('archived')) {
+        $el.show_unvotable_message();
+        return;
+      }
+
+      var $thing = $el.thing();
+      var id = $thing.thing_id();
+      var dir = $el.hasClass(UP_CLS) ? 1 : $el.hasClass(DOWN_CLS) ? -1 : 0;
+      var isTrusted;
+
+      if (!e || !e.originalEvent) {
+        isTrusted = false;
+      } else if ('isTrusted' in MouseEvent.prototype) {
+        isTrusted = e.originalEvent.isTrusted;
+      } else {
+        isTrusted = (e.originalEvent instanceof MouseEvent &&
+                     e.originalEvent !== theFakeClick);
+      }
+
+      var voteData = {
+        id: id,
+        dir: dir,
+        vh: r.config.vote_hash,
+        isTrusted: isTrusted,
+      };
+
+      $.request("vote", voteData);
+      $thing.setVoteState(dir);
+    });
+  })
+}(r);

r2/r2/public/static/js/jquery.reddit.js
@@ -262,49 +262,39 @@ rate_limit = (function() {
     };
 })()
 
+$.fn.vote = function(event) {
+    var $this = $(this);
 
-$.fn.vote = function(vh, callback, event, ui_only) {
-    /* for vote to work, $(this) should be the clicked arrow */
-    if (reddit.logged && $(this).hasClass("arrow")) {
-        var dir = ( $(this).hasClass(up_cls) ? 1 :
-                    ( $(this).hasClass(down_cls) ? -1 : 0) );
-        var things = $(this).all_things_by_id();
-        /* find all arrows of things on the page */
-        var arrows = things.children().not(".child").find('.arrow');
+    if (!r.config.logged || !$this.hasClass('arrow')) {
+        return;
+    }
+    
+    var isUpvoting = $this.hasClass(up_cls);
+    var isDownvoting = $this.hasClass(down_cls);
+    var dir = isUpvoting ? 1 : isDownvoting ? -1 : 0;
+    var thing_id = $this.thing().thing_id();
+    var $things = $.things(thing_id);
+    
+    $things.setVoteState(dir);
+    event_data = JSON.stringify(event, ["isTrusted", "type", "timeStamp"]);
+    $.request("vote", {id: thing_id, dir: dir, vh: r.config.vote_hash, event_data: event_data});
+};
 
-        /* set the new arrow states */
-        var u_before = (dir == 1) ? up_cls : upmod_cls;
-        var u_after  = (dir == 1) ? upmod_cls : up_cls;
-        arrows.filter("."+u_before).removeClass(u_before).addClass(u_after);
+$.fn.setVoteState = function(dir) {
+    var $entry = $(this).find(".entry:first, .midcol:first");
 
-        var d_before = (dir == -1) ? down_cls : downmod_cls;
-        var d_after  = (dir == -1) ? downmod_cls : down_cls;
-        arrows.filter("."+d_before).removeClass(d_before).addClass(d_after);
-
-        /* let the user vote only if they are logged in */
-        if(reddit.logged) {
-            things.each(function() {
-                    var entry =  $(this).find(".entry:first, .midcol:first");
-                    if(dir > 0)
-                        entry.addClass('likes')
-                            .removeClass('dislikes unvoted');
-                    else if(dir < 0)
-                        entry.addClass('dislikes')
-                            .removeClass('likes unvoted');
-                    else
-                        entry.addClass('unvoted')
-                            .removeClass('likes dislikes');
-                });
-            if(!$.defined(ui_only)) {
-                var thing_id = things.filter(":first").thing_id();
-                /* IE6 hack */
-                vh += event ? "" : ("-" + thing_id); 
-                $.request("vote", {id: thing_id, dir : dir, vh : vh});
-            }
-        }
-        /* execute any callbacks passed in.  */
-        if(callback) 
-            callback(things, dir);
+    if (dir > 0) {
+        $entry.addClass('likes').removeClass('dislikes unvoted')
+              .find(".arrow."+up_cls).removeClass(up_cls).addClass(upmod_cls).end()
+              .find(".arrow."+downmod_cls).removeClass(downmod_cls).addClass(down_cls);
+    } else if (dir < 0) {
+        $entry.addClass('dislikes').removeClass('likes unvoted')
+              .find(".arrow."+upmod_cls).removeClass(upmod_cls).addClass(up_cls).end()
+              .find(".arrow."+down_cls).removeClass(down_cls).addClass(downmod_cls);
+    } else {
+        $entry.addClass('unvoted').removeClass('likes dislikes')
+              .find(".arrow."+upmod_cls).removeClass(upmod_cls).addClass(up_cls).end()
+              .find(".arrow."+downmod_cls).removeClass(downmod_cls).addClass(down_cls);
     }
 };
 

r2/r2/templates/printable.html
@@ -209,14 +209,10 @@ ${self.RenderPrintable()}
    arrow_type = "up" if dir > 0 else "down"
    arrow_class = arrow_type + ("mod" if mod else "")
    login_class = "login-required" if not g.read_only_mode else ""
+   archived_class = "archived" if not getattr(thing, 'votable', True) else ""
 %>
-  <div ${classes("arrow", arrow_class, login_class)}
+  <div ${classes("arrow", arrow_class, login_class, archived_class)}
     %if not g.read_only_mode:
-       %if getattr(thing, "votable", True):
-         onclick="$(this).vote(r.config.vote_hash, null, event)"
-       %else:
-         onclick="$(this).show_unvotable_message()"
-       %endif
        role="button"
        % if dir > 0:
        aria-label="${_("upvote")}"

r2/r2/templates/thingupdater.html
@@ -29,10 +29,10 @@
     var dislikes = ${unsafe(simplejson.dumps(thing.dislikes))};
     var friends = ${unsafe(simplejson.dumps(list(thing.is_friend)))};
     $.map(likes, function(l) {
-              $(".id-" + l + " > .midcol").find(".arrow.up").vote("", null, null, true);
+              $(".id-" + l).setVoteState(1);
           });
     $.map(dislikes, function(l) {
-              $(".id-" + l + " > .midcol").find(".arrow.down").vote("", null, null, true);
+              $(".id-" + l).setVoteState(-1);
           });
     $.map(friends, show_friend);
 

commit 5edeb0c3b3e8cbe29866fe6aa3928126a91d6bc0

Voting: Replace temporary fix for IE11.
Commit d9daaaf added a temporary fix for errors introduced in commit 5c3f569 that
caused reddit to break in some browsers, most notably IE11. This improves upon
that fix by reducing the amount of code in try/catch blocks to only the known
sources of errors, and adding some safegaurds to catch other potential errors.

r2/r2/lib/js.py
@@ -439,10 +439,10 @@ module["reddit-init-base"] = LocalizedModule("reddit-init-base.js",
     "lib/jquery.cookie.js",
     "bootstrap.tooltip.extension.js",
     "base.js",
-    "voting.js",
     "preload.js",
     "logging.js",
     "client-error-logger.js",
+    "voting.js",
     "jquery.html-patch.js",
     "uibase.js",
     "i18n.js",

r2/r2/public/static/js/voting.js
@@ -2,34 +2,49 @@
   var UP_CLS = "up";
   var DOWN_CLS = "down";
   var theFakeClick;
-  var MouseEvent;
-  var createEvent;
-  
-  try {
-    MouseEvent = window.MouseEvent;
-    createEvent = document.createEvent.bind(document);
+  var MouseEvent = window.MouseEvent;
+  var createEvent = document.createEvent;
 
+  if (createEvent) {
+    // document.createEvent throws if not called from document;
+    createEvent = createEvent.bind(document);
+  }
+
+  try {
+    // Some browsers (e.g. IE11) throw an error here.
     if (MouseEvent) {
       theFakeClick = new MouseEvent('click', {bubbles: true});
-    } else if (createEvent) {
-      theFakeClick = createEvent('MouseEvent');
-    } else {
-      theFakeClick = {};
-    }
-
-    window.MouseEvent = function(type, init) {
-      return theFakeClick;
-    }
-
-    document.createEvent = function(type) {
-      if (type === 'MouseEvent' || type === 'MouseEvents') {
-        return theFakeClick;
-      } else {
-        return createEvent(type);
-      }
     }
   } catch (e) {
-    // something went wrong
+    // We'll handle this below as if MouseEvent doesn't exist.
+  }
+
+  try {
+    // To be on the safe side, we'll wrap this one in a try/catch as well.
+    // It needs to be in a separate try/catch because if creating theFakeClick
+    // with MouseEvent fails, we still want to fall back to trying with createEvent.
+    if (!theFakeClick && createEvent) {
+      theFakeClick = createEvent('MouseEvent');
+    }
+  } catch (e) {
+    // We'll handle this below as if createEvent doesn't exist.
+  }
+
+  if (!theFakeClick) {
+    // If no method for creating a custom mouse event exists, just use an object.
+    theFakeClick = {};
+  }
+
+  window.MouseEvent = function(type, init) {
+    return theFakeClick;
+  }
+
+  document.createEvent = function(type) {
+    if (type === 'MouseEvent' || type === 'MouseEvents') {
+      return theFakeClick;
+    } else {
+      return createEvent(type);
+    }
   }
 
   $(function() {
@@ -52,11 +67,14 @@
 
       if (!e || !e.originalEvent) {
         isTrusted = false;
-      } else if ('isTrusted' in MouseEvent.prototype) {
+      } else if (MouseEvent instanceof Function &&
+                 'isTrusted' in MouseEvent.prototype) {
         isTrusted = e.originalEvent.isTrusted;
-      } else if (MouseEvent) {
+      } else if (MouseEvent instanceof Function) {
         isTrusted = (e.originalEvent instanceof MouseEvent &&
                      e.originalEvent !== theFakeClick);
+      } else {
+        isTrusted = (e.originalEvent !== theFakeClick);
       }
 
       var voteData = {
@@ -69,5 +87,5 @@
       $.request("vote", voteData);
       $thing.updateThing({ voted: dir });
     });
-  })
+  });
 }(r);