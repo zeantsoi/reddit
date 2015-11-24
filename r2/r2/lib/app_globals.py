11f0405cd2c300dd2be4788205d0adef94beb168
Brian Simpson <bsimpson63@gmail.com>
2015-11-24 10:55:05 -0800

    Move pagecache behind mcrouter.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -261,7 +261,6 @@ class Globals(object):
             'memcaches',
             'lockcaches',
             'permacache_memcaches',
-            'pagecaches',
             'memoizecaches',
             'srmembercaches',
             'relcaches',
@@ -769,17 +768,6 @@ class Globals(object):
         else:
             stalecaches = None
 
-        # pagecaches hold fully rendered pages
-        pagecaches = CMemcache(
-            "page",
-            self.pagecaches,
-            noreply=True,
-            no_block=True,
-            num_clients=num_mc_clients,
-            min_compress_len=1400,
-            validators=[],
-        )
-
         # hardcache memcache pool
         hardcache_memcaches = CMemcache(
             "hardcache",
@@ -898,9 +886,10 @@ class Globals(object):
         ))
         cache_chains.update(rendercache=self.rendercache)
 
+        # pagecaches hold fully rendered pages (includes comment panes)
         self.pagecache = MemcacheChain((
             localcache_cls(),
-            pagecaches,
+            self.mcrouter,
         ))
         cache_chains.update(pagecache=self.pagecache)
 
