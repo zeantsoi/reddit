fc4d9d4d713e2c053c2c427386411520e3379626
umbrae <umbrae@reddit.com>
2015-11-25 09:22:35 -0800

    EU Cookies: Migrate to v2 cookie to normalize paths across all cookies.
    We accidentally released EU cookies to production setting a cookie
    on the current path, which meant that it would show up always for
    different paths. This is bad because the cookie has essentially been
    made permanently unreliable, such that if we don't know the path that it
    was set upon we can't clear it or update it.
    
    Here we're migrating to a v2 of the cookie, but still reading from the
    old cookie if it exists so that we don't show it to users if they've
    already accepted it from the homepage. If they see it this time, it will
    write to the new cookie and on next page load it will take the new
    cookie as the preference, which will always be set at the root path.
    
    Luckily the eu_cookie will go away over time,  as it was only a session
    cookie at the time it was set, so probably sometime in early 2016 we can
    migrate back to just eu_cookie.

--- r2/r2/public/static/js/eu-cookie-policy.js
+++ r2/r2/public/static/js/eu-cookie-policy.js
@@ -2,11 +2,34 @@
   var COOKIE_MESSAGE = _.escape(r._('Cookies help us deliver our Services. By using our Services or clicking I agree, you agree to our use of cookies.'));
 
   var required = r.config.requires_eu_cookie_policy;
-  var cookie = r.config.eu_cookie;
   var maxAttempts = r.config.eu_cookie_max_attempts;
-  var shown = parseInt($.cookie(cookie) || 0, 10);
 
-  if (required && shown < maxAttempts) {
+  /**
+   * HACK: We accidentally released EU cookies to production setting a cookie
+   * on the current path, which meant that it would show up always for
+   * different paths. This is bad because the cookie has essentially been
+   * made permanently unreliable, such that if we don't know the path that it
+   * was set upon we can't clear it or update it.
+   *
+   * Here we're migrating to a v2 of the cookie, but still reading from the
+   * old cookie if it exists so that we don't show it to users if they've
+   * already accepted it from the homepage. If they see it this time, it will
+   * write to the new cookie and on next page load it will take the new
+   * cookie as the preference, which will always be set at the root path.
+   *
+   * Luckily the eu_cookie will go away over time,  as it was only a session
+   * cookie at the time it was set, so probably sometime in early 2016 we can
+   * migrate back to just eu_cookie.
+  **/
+  var oldCookieName = "eu_cookie";
+  var newCookieName = "eu_cookie_v2";
+
+  var previousTimesShown = parseInt($.cookie(newCookieName) || 0, 10);
+  if (!previousTimesShown) {
+    previousTimesShown = parseInt($.cookie(oldCookieName) || 0, 10);
+  }
+
+  if (required && previousTimesShown < maxAttempts) {
     $(document.body).append(
         $('<form id="eu-cookie-policy">' +
             '<div class="reddit-infobar md-container-small with-icon with-btn cookie-infobar">' +
@@ -30,13 +53,13 @@
         ).on('submit', function(e) {
           e.preventDefault();
 
-          $.cookie(cookie, maxAttempts, {domain:r.config.cur_domain, path:'/', expires: 365 * 10});
+          $.cookie(newCookieName, maxAttempts, {domain:r.config.cur_domain, path:'/', expires: 365 * 10});
 
           $(this).hide();
         })
     );
 
-    $.cookie(cookie, Math.min(shown + 1, maxAttempts), {domain:r.config.cur_domain, path:'/', expires: 365 * 10});
+    $.cookie(newCookieName, Math.min(previousTimesShown + 1, maxAttempts), {domain:r.config.cur_domain, path:'/', expires: 365 * 10});
   }
 
 })(this, (this.r = this.r || {}), this.jQuery);
