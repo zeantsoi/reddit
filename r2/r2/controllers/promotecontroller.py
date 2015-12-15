zeantsoi/reddit

commit 2c64022c8aca937a875f733d716534322b322e8d

Use s3 direct POST for ads images.
This reverts commits:
  ecf70841bbd6e31150bed9b432bfaf7ccaad66b5,
  c148a8eece5fc81bea6d8a9b6e89e463dfe1c51d,
  367341b446e80f046b663a1daff65691950fc8fc,
  12cbc8cf164c94aa64831d28b3540f49b0a49af6.

r2/r2/controllers/promotecontroller.py
@@ -24,8 +24,12 @@ from datetime import datetime, timedelta
 
 from babel.dates import format_date
 from babel.numbers import format_number
+import hashlib
+import hmac
 import json
 import urllib
+import mimetypes
+import os
 
 from pylons import request
 from pylons import tmpl_context as c
@@ -35,19 +39,21 @@ from pylons.i18n import _, N_
 from r2.controllers.api import ApiController
 from r2.controllers.listingcontroller import ListingController
 from r2.controllers.reddit_base import RedditController
-
-from r2.lib import hooks, inventory, promote
+from r2.lib import (
+    hooks,
+    inventory,
+    media,
+    promote,
+    s3_helpers,
+)
 from r2.lib.authorize import get_account_info, edit_profile, PROFILE_LIMIT
 from r2.lib.base import abort
 from r2.lib.db import queries
 from r2.lib.errors import errors
-from r2.lib.filters import websafe
-from r2.lib.template_helpers import format_html
-from r2.lib.media import (
-    force_mobile_ad_image,
-    force_thumbnail,
-    thumbnail_url,
-    _scrape_media,
+from r2.lib.filters import jssafe, scriptsafe_dumps
+from r2.lib.template_helpers import (
+    add_sr,
+    format_html,
 )
 from r2.lib.memoize import memoize
 from r2.lib.menus import NamedButton, NavButton, NavMenu, QueryButton
@@ -64,11 +70,11 @@ from r2.lib.pages import (
     RenderableCampaign,
     Roadblocks,
     SponsorLookupUser,
-    UploadedImage,
 )
 from r2.lib.pages.things import default_thing_wrapper, wrap_links
 from r2.lib.system_messages import user_added_messages
 from r2.lib.utils import (
+    constant_time_compare,
     is_subdomain,
     to_date,
     to36,
@@ -101,6 +107,7 @@ from r2.lib.validator import (
     VModhash,
     VOneOf,
     VOSVersion,
+    VPrintable,
     VPriority,
     VPromoCampaign,
     VPromoTarget,
@@ -138,6 +145,35 @@ IOS_DEVICES = ('iPhone', 'iPad', 'iPod',)
 ANDROID_DEVICES = ('phone', 'tablet',)
 
 
+EXPIRES_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+ALLOWED_IMAGE_TYPES = set(["image/jpg", "image/jpeg", "image/png"])
+
+def _format_expires(expires):
+    return expires.strftime(EXPIRES_DATE_FORMAT)
+
+
+def _get_callback_hmac(username, key, expires):
+    secret = g.secrets["s3_direct_post_callback"]
+    expires_str = _format_expires(expires)
+    data = "|".join([username, key, expires_str])
+
+    return hmac.new(secret, data, hashlib.sha256).hexdigest()
+
+
+def _force_images(link, thumbnail, mobile):
+    changed = False
+
+    if thumbnail:
+        media.force_thumbnail(link, thumbnail["data"], thumbnail["ext"])
+        changed = True
+
+    if mobile:
+        media.force_mobile_ad_image(link, mobile["data"], mobile["ext"])
+        changed = True
+
+    return changed
+
+
 def campaign_has_oversold_error(form, campaign):
     if campaign.priority.inventory_override:
         return
@@ -170,11 +206,71 @@ def has_oversold_error(form, campaign, start, end, bid, cpm, target, location):
         return True
 
 
+def _key_to_dict(key, data=False):
+    timer = g.stats.get_timer("providers.s3.get_ads_key_meta.with_%s" %
+        ("data" if data else "no_data"))
+    timer.start()
+
+    url = key.generate_url(expires_in=0, query_auth=False)
+    # Generating an S3 url without authentication fails for IAM roles.
+    # This removes the bad query params.
+    # see: https://github.com/boto/boto/issues/2043
+    url = promote.update_query(url, {"x-amz-security-token": None}, unset=True)
+
+    result = {
+        "url": url,
+        "data": key.get_contents_as_string() if data else None,
+        "ext": key.get_metadata("ext"),
+    }
+
+    timer.stop()
+
+    return result
+
+
+def _get_ads_keyspace(thing):
+    return "ads/%s/" % thing._fullname
+
+
+def _get_ads_images(thing, data=False, **kwargs):
+    images = {}
+
+    timer = g.stats.get_timer("providers.s3.get_ads_image_keys")
+    timer.start()
+
+    keys = s3_helpers.get_keys(g.s3_client_uploads_bucket, prefix=_get_ads_keyspace(thing), **kwargs)
+
+    timer.stop()
+
+    for key in keys:
+        filename = os.path.basename(key.key)
+        name, ext = os.path.splitext(filename)
+
+        if name not in ("mobile", "thumbnail"):
+            continue
+
+        images[name] = _key_to_dict(key, data=data)
+
+    return images
+
+
+def _clear_ads_images(thing):
+    timer = g.stats.get_timer("providers.s3.delete_ads_image_keys")
+    timer.start()
+
+    s3_helpers.delete_keys(g.s3_client_uploads_bucket, prefix=_get_ads_keyspace(thing))
+
+    timer.stop()
+
+
 class PromoteController(RedditController):
     @validate(VSponsor())
     def GET_new_promo(self):
+        ads_images = _get_ads_images(c.user)
+        images = {k: v.get("url") for k, v in ads_images.iteritems()}
+
         return PromotePage(title=_("create sponsored link"),
-                           content=PromoteLinkNew(),
+                           content=PromoteLinkNew(images),
                            extra_js_config={
                             "ads_virtual_page": "new-promo",
                            }).render()
@@ -667,7 +763,7 @@ class PromoteApiController(ApiController):
         else:
             form.set_text('.status', _('refund not needed'))
 
-    @validatedMultipartForm(
+    @validatedForm(
         VSponsor('link_id36'),
         VModhash(),
         VRatelimit(rate_user=True,
@@ -692,20 +788,25 @@ class PromoteApiController(ApiController):
         third_party_tracking=VUrl("third_party_tracking"),
         third_party_tracking_2=VUrl("third_party_tracking_2"),
         is_managed=VBoolean("is_managed"),
-        thumbnail_file=VUploadLength('file', 500*1024),
     )
     def POST_create_promo(self, form, jquery, username, title, url,
                           selftext, kind, disable_comments, sendreplies,
                           media_url, media_autoplay, media_override,
                           iframe_embed_url, media_url_type, domain_override,
                           third_party_tracking, third_party_tracking_2,
-                          is_managed, thumbnail_file):
-        return self._edit_promo(form, jquery, username, title, url,
-                                selftext, kind, disable_comments, sendreplies,
-                                media_url, media_autoplay, media_override,
-                                iframe_embed_url, media_url_type, domain_override,
-                                third_party_tracking, third_party_tracking_2,
-                                is_managed, thumbnail_file=thumbnail_file)
+                          is_managed):
+
+        images = _get_ads_images(c.user, data=True, meta=True)
+
+        return self._edit_promo(
+            form, jquery, username, title, url,
+            selftext, kind, disable_comments, sendreplies,
+            media_url, media_autoplay, media_override,
+            iframe_embed_url, media_url_type, domain_override,
+            third_party_tracking, third_party_tracking_2, is_managed,
+            thumbnail=images.get("thumbnail", None),
+            mobile=images.get("mobile", None),
+        )
 
     @validatedForm(
         VSponsor('link_id36'),
@@ -740,22 +841,30 @@ class PromoteApiController(ApiController):
                         iframe_embed_url, media_url_type, domain_override,
                         third_party_tracking, third_party_tracking_2,
                         is_managed, l):
-        return self._edit_promo(form, jquery, username, title, url,
-                                selftext, kind, disable_comments, sendreplies,
-                                media_url, media_autoplay, media_override,
-                                iframe_embed_url, media_url_type, domain_override,
-                                third_party_tracking, third_party_tracking_2,
-                                is_managed, l=l)
+
+        images = _get_ads_images(l, data=True, meta=True)
+
+        return self._edit_promo(
+            form, jquery, username, title, url,
+            selftext, kind, disable_comments, sendreplies,
+            media_url, media_autoplay, media_override,
+            iframe_embed_url, media_url_type, domain_override,
+            third_party_tracking, third_party_tracking_2, is_managed,
+            l=l,
+            thumbnail=images.get("thumbnail", None),
+            mobile=images.get("mobile", None),
+        )
 
     def _edit_promo(self, form, jquery, username, title, url,
                     selftext, kind, disable_comments, sendreplies,
                     media_url, media_autoplay, media_override,
                     iframe_embed_url, media_url_type, domain_override,
                     third_party_tracking, third_party_tracking_2,
-                    is_managed, l=None, thumbnail_file=None):
+                    is_managed, l=None, thumbnail=None, mobile=None):
         should_ratelimit = False
         is_self = (kind == "self")
         is_link = not is_self
+        is_new_promoted = not l
         if not c.user_is_sponsor:
             should_ratelimit = True
 
@@ -763,7 +872,7 @@ class PromoteApiController(ApiController):
             c.errors.remove((errors.RATELIMIT, 'ratelimit'))
 
         # check for user override
-        if not l and c.user_is_sponsor and username:
+        if is_new_promoted and c.user_is_sponsor and username:
             try:
                 user = Account._by_name(username)
             except NotFound:
@@ -803,7 +912,7 @@ class PromoteApiController(ApiController):
                 return
 
         # users can change the disable_comments on promoted links
-        if ((not l or not promote.is_promoted(l)) and
+        if ((is_new_promoted or not promote.is_promoted(l)) and
             (form.has_errors('title', errors.NO_TEXT, errors.TOO_LONG) or
              jquery.has_errors('ratelimit', errors.RATELIMIT))):
             return
@@ -811,7 +920,7 @@ class PromoteApiController(ApiController):
         if is_self and form.has_errors('text', errors.TOO_LONG):
             return
 
-        if not l:
+        if is_new_promoted:
             # creating a new promoted link
             l = promote.new_promotion(
                 is_self=is_self,
@@ -829,13 +938,7 @@ class PromoteApiController(ApiController):
                     l.third_party_tracking_2 = third_party_tracking_2 or None
             l._commit()
 
-            # only set the thumbnail when creating a link
-            if thumbnail_file:
-                try:
-                    force_thumbnail(l, thumbnail_file)
-                    l._commit()
-                except IOError:
-                    pass
+            _force_images(l, thumbnail=thumbnail, mobile=mobile)
 
             form.redirect(promote.promo_edit_url(l))
 
@@ -848,6 +951,9 @@ class PromoteApiController(ApiController):
         if not promote.is_promoted(l) or c.user_is_sponsor:
             if title and title != l.title:
                 l.title = title
+                changed = True
+
+            if _force_images(l, thumbnail=thumbnail, mobile=mobile):
                 changed = True
 
             # type changing
@@ -882,13 +988,13 @@ class PromoteApiController(ApiController):
 
         if c.user_is_sponsor and scraper_embed and media_url != l.media_url:
             if media_url:
-                media = _scrape_media(
+                scraped = media._scrape_media(
                     media_url, autoplay=media_autoplay,
                     save_thumbnail=False, use_cache=True)
 
-                if media:
-                    l.set_media_object(media.media_object)
-                    l.set_secure_media_object(media.secure_media_object)
+                if scraped:
+                    l.set_media_object(scraped.media_object)
+                    l.set_secure_media_object(scraped.secure_media_object)
                     l.media_url = media_url
                     l.iframe_embed_url = None
                     l.media_autoplay = media_autoplay
@@ -946,6 +1052,11 @@ class PromoteApiController(ApiController):
                 l.third_party_tracking_2 = third_party_tracking_2 or None
 
         l._commit()
+
+        # clean up so the same images don't reappear if they create
+        # another link
+        _clear_ads_images(thing=c.user if is_new_promoted else l)
+
         form.redirect(promote.promo_edit_url(l))
 
     @validatedForm(
@@ -1318,36 +1429,77 @@ class PromoteApiController(ApiController):
             promote.failed_payment_method(c.user, link)
             form.set_text(".status", _("failed to authenticate card. sorry."))
 
-    @validate(
-        VSponsor("link_name"),
+    @json_validate(
+        VSponsor("link"),
         VModhash(),
-        link=VByName('link_name'),
-        file=VUploadLength('file', 500*1024),
-        img_type=VImageType('img_type'),
+        link=VLink("link"),
+        kind=VOneOf("kind", ["thumbnail", "mobile"]),
+        filepath=nop("filepath"),
+        ajax=VBoolean("ajax", default=True)
     )
-    def POST_link_thumb(self, link=None, file=None, img_type='jpg'):
-        if not link or (promote.is_promoted(link) and not c.user_is_sponsor):
-            # only let sponsors edit thumbnails of live promos
-            return abort(403, 'forbidden')
+    def POST_ad_s3_params(self, responder, link, kind, filepath, ajax):
+        filename, ext = os.path.splitext(filepath)
+        mime_type, encoding = mimetypes.guess_type(filepath)
 
-        force_thumbnail(link, file, file_type=".%s" % img_type)
-        link._commit()
-        return UploadedImage(_('saved'), thumbnail_url(link), "", errors=errors,
-                             form_id="image-upload").render()
+        if not mime_type or mime_type not in ALLOWED_IMAGE_TYPES:
+            request.environ["extra_error_data"] = {
+                "message": _("image must be a jpg or png"),
+            }
+            abort(403)
+
+        keyspace = _get_ads_keyspace(link if link else c.user)
+        key = os.path.join(keyspace, kind)
+        redirect = None
+
+        if not ajax:
+            now = datetime.now().replace(tzinfo=g.tz)
+            signature = _get_callback_hmac(
+                username=c.user.name,
+                key=key,
+                expires=now,
+            )
+            path = ("/api/ad_s3_callback?hmac=%s&ts=%s" %
+                (signature, _format_expires(now)))
+            redirect = add_sr(path, sr_path=False)
+
+        return s3_helpers.get_post_args(
+            bucket=g.s3_client_uploads_bucket,
+            key=key,
+            success_action_redirect=redirect,
+            success_action_status="201",
+            content_type=mime_type,
+            meta={
+                "x-amz-meta-ext": ext,
+            },
+        )
 
     @validate(
-        VSponsor("link_name"),
-        VModhash(),
-        link=VByName('link_name'),
-        file=VUploadLength('file', 500*1024),
-        img_type=VImageType('img_type'),
+        VSponsor(),
+        expires=VDate("ts", format=EXPIRES_DATE_FORMAT),
+        signature=VPrintable("hmac", 255),
+        callback=nop("callback"),
+        key=nop("key"),
     )
-    def POST_link_mobile_ad_image(self, link=None, file=None, img_type='jpg'):
-        if not (link and c.user_is_sponsor and file):
-            # only sponsors can set the mobile img
-            return abort(403, 'forbidden')
+    def GET_ad_s3_callback(self, expires, signature, callback, key):
+        now = datetime.now(tz=g.tz)
+        if (expires + timedelta(minutes=10) < now):
+            self.abort404()
 
-        force_mobile_ad_image(link, file, file_type=".%s" % img_type)
-        link._commit()
-        return UploadedImage(_('saved'), link.mobile_ad_url, "", errors=errors,
-                             form_id="mobile-ad-image-upload").render()
+        expected_mac = _get_callback_hmac(
+            username=c.user.name,
+            key=key,
+            expires=expires,
+        )
+
+        if not constant_time_compare(signature, expected_mac):
+            self.abort404()
+
+        template = "<script>parent.__s3_callbacks__[%(callback)s](%(data)s);</script>"
+        image = _key_to_dict(
+            s3_helpers.get_key(g.s3_client_uploads_bucket, key))
+        response = {
+            "callback": scriptsafe_dumps(callback),
+            "data": scriptsafe_dumps(image),
+        }
+
+        return format_html(template, response)

r2/r2/config/routing.py
@@ -389,12 +389,12 @@ def make_map(config):
     mc('/api/gadget/:type', controller='api', action='gadget')
     mc('/api/modmailreply', controller='mailgunwebhook', action='modmailreply')
     mc('/api/:action', controller='promoteapi',
-       requirements=dict(action=("promote|unpromote|edit_promo|link_thumb|"
-                                 "freebie|promote_note|update_pay|"
+       requirements=dict(action=("promote|unpromote|edit_promo|ad_s3_callback|"
+                                 "ad_s3_params|freebie|promote_note|update_pay|"
                                  "edit_campaign|delete_campaign|"
                                  "add_roadblock|rm_roadblock|check_inventory|"
                                  "refund_campaign|terminate_campaign|"
-                                 "review_fraud|create_promo|link_mobile_ad_image")))
+                                 "review_fraud|create_promo")))
     mc('/api/:action', controller='apiminimal',
        requirements=dict(action="new_captcha"))
     mc('/api/:type', controller='api',

r2/r2/lib/js.py
@@ -546,6 +546,7 @@ module["sponsored"] = LocalizedModule("sponsored.js",
     "lib/ui.core.js",
     "lib/ui.datepicker.js",
     "lib/react-with-addons-0.11.2.js",
+    "image-upload.js",
     "sponsored.js"
 )
 

r2/r2/lib/pages/pages.py
@@ -4288,7 +4288,10 @@ class PromoteLinkBase(Templated):
 
 
 class PromoteLinkNew(PromoteLinkBase):
-    pass
+    def __init__(self, images=None, *a, **kw):
+        images = images or {}
+        self.images = images
+        PromoteLinkBase.__init__(self, **kw)
 
 
 class PromoteLinkEdit(PromoteLinkBase):

r2/r2/lib/promote.py
@@ -180,10 +180,14 @@ def is_pending(campaign):
     today = promo_datetime_now().date()
     return today < to_date(campaign.start_date)
 
-def update_query(base_url, query_updates):
+def update_query(base_url, query_updates, unset=False):
     scheme, netloc, path, params, query, fragment = urlparse.urlparse(base_url)
     query_dict = urlparse.parse_qs(query)
     query_dict.update(query_updates)
+
+    if unset:
+        query_dict = dict((k, v) for k, v in query_dict.iteritems() if v is not None)
+
     query = urllib.urlencode(query_dict, doseq=True)
     return urlparse.urlunparse((scheme, netloc, path, params, query, fragment))
 

r2/r2/lib/s3_helpers.py
@@ -20,10 +20,17 @@
 # Inc. All Rights Reserved.
 ###############################################################################
 
+import base64
+import boto
+import hashlib
+import hmac
+import json
 import os
 import sys
+import time
 
-from boto.s3.key import Key
+from pylons import app_globals as g
+
 
 HADOOP_FOLDER_SUFFIX = '_$folder$'
 
@@ -62,7 +69,7 @@ def get_text_from_s3(s3_connection, path):
     """Read a file from S3 and return it as text."""
     bucket_name, key_name = _from_path(path)
     bucket = s3_connection.get_bucket(bucket_name)
-    k = Key(bucket)
+    k = boto.s3.Key(bucket)
     k.key = key_name
     txt = k.get_contents_as_string()
     return txt
@@ -74,7 +81,7 @@ def mv_file_s3(s3_connection, src_path, dst_path):
     dst_bucket_name, dst_key_name = _from_path(dst_path)
 
     src_bucket = s3_connection.get_bucket(src_bucket_name)
-    k = Key(src_bucket)
+    k = boto.s3.Key(src_bucket)
     k.key = src_key_name
     k.copy(dst_bucket_name, dst_key_name)
     k.delete()
@@ -100,7 +107,7 @@ def copy_to_s3(s3_connection, local_path, dst_path, verbose=False):
         return
 
     key_name = os.path.join(dst_key_name, filename)
-    k = Key(bucket)
+    k = boto.s3.Key(bucket)
     k.key = key_name
 
     kw = {}
@@ -109,3 +116,187 @@ def copy_to_s3(s3_connection, local_path, dst_path, verbose=False):
         kw['cb'] = callback
 
     k.set_contents_from_filename(logfile, **kw)
+
+
+def get_connection():
+    return boto.connect_s3(g.S3KEY_ID or None, g.S3SECRET_KEY or None)
+
+
+def get_key(bucket_name, key, connection=None):
+    connection = connection or get_connection()
+    bucket = connection.get_bucket(bucket_name)
+
+    return bucket.get_key(key)
+
+def get_keys(bucket_name, meta=False, connection=None, **kwargs):
+    connection = connection or get_connection()
+    bucket = connection.get_bucket(bucket_name)
+    keys = bucket.get_all_keys(**kwargs)
+
+    if not meta:
+        return keys
+
+    return [bucket.get_key(key.name)
+            for key in keys]
+
+
+def delete_keys(bucket_name, prefix, connection=None):
+    connection = connection or get_connection()
+
+    keys = get_keys(bucket_name, prefix=prefix, connection=connection)
+    return connection.get_bucket(bucket_name).delete_keys(keys)
+
+
+def _get_upload_policy(
+        bucket, key, acl, ttl=60,
+        success_action_redirect=None,
+        success_action_status="201",
+        content_type=None,
+        max_content_length=((1024**2) * 3),
+        storage_class="STANDARD",
+        meta=None,
+        connection=None,
+    ):
+
+    connection = connection or get_connection()
+    meta = meta or {}
+
+    expiration = time.gmtime(int(time.time() + ttl))
+    conditions = []
+
+    conditions.append({"bucket": bucket})
+
+    if key.endswith("${filename}"):
+        conditions.append(["starts-with", "$key", key[:-len("${filename}")]])
+    else:
+        conditions.append({"key": key})
+
+    conditions.append({"acl": acl})
+    conditions.append({"x-amz-storage-class": storage_class})
+
+    if success_action_redirect:
+        conditions.append([
+            "starts-with",
+            "$success_action_redirect",
+            success_action_redirect,
+        ])
+    else:
+        conditions.append({
+            "success_action_status": success_action_status,
+        })
+
+    conditions.append([
+        "content-length-range", 0, max_content_length])
+
+    for key, value in meta.iteritems():
+        conditions.append({key: value})
+
+    if content_type:
+        conditions.append({"content-type": content_type})
+
+    return base64.b64encode(json.dumps({
+        "expiration": time.strftime(boto.utils.ISO8601, expiration),
+        "conditions": conditions,
+    }))
+
+
+def _get_upload_signature(
+        policy,
+        connection=None,
+    ):
+
+    connection = connection or get_connection()
+
+    key = connection.provider.secret_key.encode("utf-8")
+    hashed = hmac.new(key, policy, hashlib.sha1)
+    return base64.encodestring(
+        hashed.digest()).decode("utf-8").strip()
+
+
+def get_post_args(
+        bucket, key,
+        acl="public-read",
+        success_action_redirect=None,
+        success_action_status="201",
+        content_type=None,
+        storage_class="STANDARD",
+        meta=None,
+        connection=None,
+        **kwargs
+    ):
+
+    meta = meta or []
+    connection = connection or get_connection()
+    policy = _get_upload_policy(
+        bucket=bucket,
+        key=key,
+        acl=acl,
+        success_action_redirect=success_action_redirect,
+        success_action_status=success_action_status,
+        content_type=content_type,
+        storage_class=storage_class,
+        meta=meta,
+        connection=connection,
+    )
+    signature = _get_upload_signature(
+        policy, connection=connection)
+
+    fields = []
+
+    fields.append({
+        "name": "AWSAccessKeyId",
+        "value": connection.provider.access_key,
+    })
+
+    fields.append({
+        "name": "acl",
+        "value": acl,
+    })
+
+    fields.append({
+        "name": "key",
+        "value": key,
+    })
+
+    if success_action_redirect:
+        fields.append({
+            "name": "success_action_redirect",
+            "value": success_action_redirect,
+        })
+    else:
+        fields.append({
+            "name": "success_action_status",
+            "value": success_action_status,
+        })
+
+    fields.append({
+        "name": "content-type",
+        "value": content_type,
+    })
+
+    fields.append({
+        "name": "x-amz-storage-class",
+        "value": storage_class,
+    })
+
+    for key, value in meta.iteritems():
+        fields.append({
+            "name": key,
+            "value": value,
+        })
+
+    fields.append({
+        "name": "policy",
+        "value": policy,
+    })
+
+    fields.append({
+        "name": "signature",
+        "value": signature,
+    })
+
+    return {
+        "action": "//%s.%s" % (bucket, g.s3_media_domain),
+        "fields": fields,
+    }
+

commit 14623281afb567dd733a40683731582686252820

Fix missing import
Regression caused by 2c64022c8aca937a875f733d716534322b322e8d.

r2/r2/controllers/promotecontroller.py
@@ -56,7 +56,11 @@ from r2.lib.authorize.api import AuthorizeNetException
 from r2.lib.base import abort
 from r2.lib.db import queries
 from r2.lib.errors import errors
-from r2.lib.filters import jssafe, scriptsafe_dumps
+from r2.lib.filters import (
+    jssafe,
+    scriptsafe_dumps,
+    websafe,
+)
 from r2.lib.template_helpers import (
     add_sr,
     format_html,