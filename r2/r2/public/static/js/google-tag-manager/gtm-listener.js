62c02e4573061526e9a355c6d33bb5e7b358b6e3
David Wick <wick.d.spencer@gmail.com>
2016-01-19 15:13:56 -0800

    GTM: Reference the data layer directly from `window`
    It seems when GTM is loaded only the global reference is hydrated
    with the proper `push` method.

--- r2/r2/public/static/js/google-tag-manager/gtm-listener.js
+++ r2/r2/public/static/js/google-tag-manager/gtm-listener.js
@@ -1,14 +1,14 @@
 ;(function(gtm, global, r, undefined) {
-  var dataLayer = global.googleTagManager = global.googleTagManager || [];
+  global.googleTagManager = global.googleTagManager || [];
 
   r.frames.listen('gtm');
 
   r.frames.receiveMessage('data.gtm', function(e) {
-    dataLayer.push(e.detail);
+    global.googleTagManager.push(e.detail);
   });
 
   r.frames.receiveMessage('event.gtm', function(e) {
-    dataLayer.push(e.detail);
+    global.googleTagManager.push(e.detail);
   });
 
 })((this.gtm = this.gtm || {}), this, this.r);
