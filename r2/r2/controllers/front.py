8a5a54ff886e8be8fbf7117c21cfab213c362426
Brian Simpson <bsimpson63@gmail.com>
2016-02-01 11:16:19 -0800

    Revert "Set attributes on Wrapped search result"
    This reverts commit ea6ac268245c374f1deaf5d7d9927de479a851c4.

--- r2/r2/controllers/front.py
+++ r2/r2/controllers/front.py
@@ -1305,11 +1305,10 @@ class FrontController(RedditController):
         recent = q.recent
         sort = q.sort
         def wrapper_fn(thing):
+            thing.prev_search = query
+            thing.recent = recent
+            thing.sort = sort
             w = Wrapped(thing)
-            w.prev_search = query
-            w.recent = recent
-            w.sort = sort
-
             if isinstance(thing, Link):
                 w.render_class = SearchResultLink
             elif isinstance(thing, Subreddit):
