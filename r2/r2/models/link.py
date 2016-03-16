3ee83141721043c31530b74bbd25f268b30b7980
MelissaCole <melissa.engineer@gmail.com>
2016-03-16 15:14:32 -0700

    Revert "Enable clicktracking for logged out users"
    This reverts commit 29b0512f6987a9b0edc998c4160785946357600d.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -814,8 +814,7 @@ class Link(Thing, Printable):
                 item.href_url = item.permalink
             else:
                 item.href_url = item.url
-                if (feature.is_enabled('outbound_clicktracking') or
-                        feature.is_enabled('outbound_clicktracking_logged_out')):
+                if feature.is_enabled('outbound_clicktracking'):
                     item.use_outbound = True
                     item.outbound_link = generate_outbound_link(item, item.url)
 
