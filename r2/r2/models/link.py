ec988a8294567d7d0dc54baf000974d8ac64f116
Chad Birch <chad.birch@gmail.com>
2016-03-17 11:57:53 -0600

    Self posts: start giving karma
    This commit starts giving and tracking karma for self-posts as well as
    link posts. The karma is stored separately internally, but displayed and
    otherwise treated as part of link karma. This will allow us to easily
    revert this change and go back to the original link-only karma values if
    desired.

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -181,8 +181,11 @@ class Link(Thing, Printable):
 
     @property
     def affects_karma_type(self):
-        if self.is_self or self.is_promoted_post:
+        if self.is_promoted_post:
             return None
+
+        if self.is_self:
+            return "self"
 
         return "link"
 
