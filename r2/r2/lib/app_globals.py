7e1471f74224184220da418e4a8ed79a2ec2df51
Brian Simpson <bsimpson63@gmail.com>
2016-03-29 14:17:43 -0700

    new_vote: Don't queue a commentstree_q update on every vote
    When a comment has received enough votes it will only update periodically.

--- r2/r2/lib/app_globals.py
+++ r2/r2/lib/app_globals.py
@@ -363,6 +363,8 @@ class Globals(object):
             'ticket_contact_user_id',
             'ads_popularity_threshold',
             'precomputed_comment_sort_min_comments',
+            'comment_vote_update_threshold',
+            'comment_vote_update_period',
         ],
         ConfigValue.float: [
             'cflag_lower_bound',
