efb4f219e7c6ec8403381fa028dc21012d114daa
adam <adam@reddit.com>
2016-04-28 10:01:16 -0700

    Email experiment: update experiment name for another try

--- r2/r2/lib/pages/pages.py
+++ r2/r2/lib/pages/pages.py
@@ -1549,8 +1549,8 @@ class Login(Templated):
                            dest = dest, captcha = Captcha(),
                            is_popup=is_popup,
                            registration_info=RegistrationInfo())
-        if (feature.is_enabled('register_email_text_v2') and 
-            feature.variant('register_email_text_v2') == 'test_group'):
+        if (feature.is_enabled('register_email_text_v3') and 
+            feature.variant('register_email_text_v3') == 'test_group'):
             self.register_email_text_test = True
 
 class Register(Login):
