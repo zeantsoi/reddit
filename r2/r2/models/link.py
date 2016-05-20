5835d0a5749e570546f1bdbb2018eb3496847c32
Brian Simpson <bsimpson63@gmail.com>
2016-05-20 14:26:00 -0700

    Stop writing Message objects to g.cache

--- r2/r2/models/link.py
+++ r2/r2/models/link.py
@@ -2092,7 +2092,7 @@ class MoreChildren(MoreComments):
 
 
 class Message(Thing, Printable):
-    _cache = g.message_transitionalcache
+    _cache = g.thingcache
     _defaults = dict(reported=0,
                      was_comment=False,
                      parent_id=None,
@@ -2112,6 +2112,10 @@ class Message(Thing, Printable):
     _data_int_props = Thing._data_int_props + ('reported',)
     _essentials = ('author_id',)
     cache_ignore = set(["to", "subreddit"]).union(Printable.cache_ignore)
+
+    @classmethod
+    def _cache_prefix(cls):
+        return "message:"
 
     @classmethod
     def _new(cls, author, to, subject, body, ip, parent=None, sr=None,
