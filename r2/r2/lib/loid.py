0794ea4121f57e648cba0f72b92316369f7e8be7
Chris Slowe <chris@reddit.com>
2016-06-06 15:37:58 -0700

    Add forward compatibility to v1 LoId spec (at least for the id)

--- r2/r2/lib/loid.py
+++ r2/r2/lib/loid.py
@@ -69,7 +69,8 @@ class LoId(object):
             return cls._create(request, context)
         elif loid:
             louser = cls()
-            louser.loid = unquote(loid)
+            # future-proof to v1 id tracking
+            louser.loid, _, _ = unquote(loid).partition(".")
             louser.created = ensure_unquoted(
                 request.cookies.get(LOID_CREATED_COOKIE, ""))
             return louser
