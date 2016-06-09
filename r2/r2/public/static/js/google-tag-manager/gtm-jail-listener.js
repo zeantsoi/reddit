c130365e34193be15da3b67bf3d3dabf348aff5b
David Wick <wick.d.spencer@gmail.com>
2016-06-09 11:34:45 -0700

    Queue events sent to GTM before the iframe is loaded.
    This reverts commit 29481b60f33639c0129acfc3ad27fa5a5c4ad6bb.

--- r2/r2/public/static/js/google-tag-manager/gtm-jail-listener.js
+++ r2/r2/public/static/js/google-tag-manager/gtm-jail-listener.js
@@ -1,13 +1,5 @@
 ;(function(global, r, undefined) {
   var jail = document.createElement('iframe');
-  var queue = [];
-  var preload = r.frames.receiveMessage('*.gtm', function(e) {
-    queue.push({
-      type: e.originalType,
-      options: e.options,
-      data: e.detail,
-    });
-  });
 
   jail.style.display = 'none';
   jail.referrer = 'no-referrer';
@@ -15,19 +7,6 @@
   jail.name = window.name;
   jail.src = '/gtm?id=' + global.CONTAINER_ID +
     '&cb=' + global.CACHE_BUSTER;
-  jail.onload = function() {
-    preload.off();
-
-    for (var i = 0; i < queue.length; i++) {
-      var queuedEvent = queue[i];
-      r.frames.postMessage(
-        jail.contentWindow,
-        queuedEvent.type,
-        queuedEvent.data,
-        queuedEvent.options
-      );
-    }
-  };
 
   document.body.appendChild(jail);
 
