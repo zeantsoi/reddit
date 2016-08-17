05c87d19fc0032d3d7c1d65863f949a20efdb09d
Chris Slowe <chris@reddit.com>
2016-08-17 14:56:39 -0700

    Fix filtering on /me/f/all/top (Patch didn't survive the deploy)
    This reverts commit 6a1350a3d449162d3e33f9e8808bff030cf4f150.

--- r2/r2/controllers/listingcontroller.py
+++ r2/r2/controllers/listingcontroller.py
@@ -665,16 +665,16 @@ class BrowseController(ListingWithPromos):
     def keep_fn(self):
         """For merged time-listings, don't show items that are too old
            (this can happen when mr_top hasn't run in a while)"""
-        if self.time != 'all' and c.default_sr:
-            oldest = timeago('1 %s' % (str(self.time),))
-            def keep(item):
-                if isinstance(c.site, AllSR):
-                    if not item.subreddit.discoverable:
-                        return False
+        def keep(item):
+            # see if we'd keep the item normally
+            if not ListingController.keep_fn(self)(item):
+                return False
+            # check if the item is too old
+            elif self.time != 'all' and c.default_sr:
+                oldest = timeago('1 %s' % (str(self.time),))
                 return item._date > oldest and item.keep_item(item)
-            return keep
-        else:
-            return ListingController.keep_fn(self)
+            return True
+        return keep
 
     @property
     def menus(self):
