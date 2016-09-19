f3d9dce573d15b5b206a1b4315efe92ccbb7a04a
MelissaCole <melissa.engineer@gmail.com>
2016-09-19 12:05:39 -0700

    Fixup - db_disallow_writes = disallow_db_writes

--- r2/r2/controllers/front.py
+++ r2/r2/controllers/front.py
@@ -267,7 +267,7 @@ class FrontController(RedditController):
             # If this is the first time someone views this thread with "live"
             # sort, turn on the allow_live_comments attr so that new comments
             # will get added to the live_comments_q
-            if not g.db_disallow_writes and not article.allow_live_comments:
+            if not g.disallow_db_writes and not article.allow_live_comments:
                 article.allow_live_comments = True
                 article._commit()
 
